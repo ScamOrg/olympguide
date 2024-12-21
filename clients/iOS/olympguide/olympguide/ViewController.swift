cb653776ecf4d2aa656c1a29298959cf1568c931
sundayti <pankratovvlad1@gmail.com>
2024-12-22 02:32:25 +0300

    Create iOS client

---
+++ clients/iOS/olympguide/olympguide/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  olympguide
+//
+//  Created by Tom Tim on 22.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
