119c0e9e33409ee83fcd40f28e428f930eaaabe0
sundayti <pankratovvlad1@gmail.com>
2024-12-22 16:14:39 +0300

    Create UniversitiesViewController

---
+++ clients/iOS/olympguide/olympguide/Application/Scenes/UniversitiesScene/UniversitiesViewController.swift
@@ -0,0 +1,17 @@
+//
+//  UniversitiesViewController.swift
+//  olympguide
+//
+//  Created by Tom Tim on 22.12.2024.
+//
+
+import UIKit
+
+class UniversitiesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
