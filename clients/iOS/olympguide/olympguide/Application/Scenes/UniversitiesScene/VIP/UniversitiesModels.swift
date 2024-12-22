8e58347f919e2cfe9ce45bf106646153ef301464
sundayti <pankratovvlad1@gmail.com>
2024-12-23 00:32:16 +0300

    Create Models

--- clients/iOS/olympguide/olympguide/Application/Scenes/UniversitiesScene/VIP/UniversitiesModels.swift
+++ clients/iOS/olympguide/olympguide/Application/Scenes/UniversitiesScene/VIP/UniversitiesModels.swift
@@ -5,10 +5,54 @@
 //  Created by Tom Tim on 22.12.2024.
 //
 
+// MARK: - Главный экран: Университеты
 enum Universities {
+    
+    // MARK: - Use Cases
     enum Load {
         struct Request {
-            
+            let regionID: Int?
+            let sortOption: SortOption?
+            let searchQuery: String?
         }
+        
+        struct Response {
+            let universities: [University]
+        }
+        
+        struct ViewModel {
+            struct UniversityViewModel {
+                let name: String
+                let logoURL: String
+                let description: String
+                let popularity: String
+            }
+            
+            let universities: [UniversityViewModel] 
+        }
+    }
+    
+    // MARK: - Опции сортировки
+    enum SortOption: String {
+        case name
+        case popularity
+    }
+}
+
+
+struct University: Codable {
+    let universityID: Int
+    let name: String
+    let logo: String
+    let description: String
+    let regionID: Int
+    let popularity: Int
+    let link: String
+
+    enum CodingKeys: String, CodingKey {
+        case universityID = "university_id"
+        case name, logo, description
+        case regionID = "region_id"
+        case popularity, link
     }
 }
