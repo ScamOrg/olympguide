7658629a46a3184297545d5f5c14acc3b8678ff0
sundayti <pankratovvlad1@gmail.com>
2024-12-23 00:55:05 +0300

    Create Protocols

--- clients/iOS/olympguide/olympguide/Application/Scenes/UniversitiesScene/VIP/UniversitiesProtocols.swift
+++ clients/iOS/olympguide/olympguide/Application/Scenes/UniversitiesScene/VIP/UniversitiesProtocols.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+protocol UniversitiesBusinessLogic {
+    func loadUniversities(_ request: Universities.Load.Request)
+}
+
+protocol UniversitiesPresentationLogic {
+    func presentUniversities(response: Universities.Load.Response)
+    func presentError(message: String)
+}
+
+protocol UniversitiesDisplayLogic: AnyObject {
+    func displayUniversities(viewModel: Universities.Load.ViewModel)
+    func displayError(message: String)
+}
+
+protocol UniversitiesRoutingLogic {
+    func routeToDetails(for university: University)
+}
