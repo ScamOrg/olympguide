2055c01ea7fbe6cfa520258369b2b5328a2a728d
titarenko <arseniytitarenko93@yandex.ru>
2024-12-25 16:53:34 +0300

    PLAN-11: add endpoint /fields and /field/:id

--- api/models/models.go
+++ api/models/models.go
@@ -9,6 +9,21 @@ type Olympiad struct {
 	Link        string `json:"link"`
 }
 
-func (Olympiad) TableName() string {
-	return "olympguide.olympiad"
+type Field struct {
+	FieldID uint   `json:"-"`
+	Name    string `json:"name"`
+	Code    string `json:"code"`
+	Degree  string `json:"degree"`
+	GroupID uint   `json:"-"`
 }
+
+type GroupField struct {
+	GroupID uint    `json:"-"`
+	Name    string  `json:"name"`
+	Code    string  `json:"code"`
+	Fields  []Field `json:"fields" gorm:"foreignKey:GroupID;references:GroupID"`
+}
+
+func (Olympiad) TableName() string   { return "olympguide.olympiad" }
+func (GroupField) TableName() string { return "olympguide.group_of_fields" }
+func (Field) TableName() string      { return "olympguide.field_of_study" }
