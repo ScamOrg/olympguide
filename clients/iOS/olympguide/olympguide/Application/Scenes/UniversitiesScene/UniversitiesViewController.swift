54295a4419d68d8e434b0fd46347b649000875a3
sundayti <pankratovvlad1@gmail.com>
2024-12-27 23:59:17 +0300

    Update UniversitiesViewController (added a label and a table of universities)

--- clients/iOS/olympguide/olympguide/Application/Scenes/UniversitiesScene/UniversitiesViewController.swift
+++ clients/iOS/olympguide/olympguide/Application/Scenes/UniversitiesScene/UniversitiesViewController.swift
@@ -7,11 +7,139 @@
 
 import UIKit
 
-class UniversitiesViewController: UIViewController {
-
+class UniversitiesViewController: UIViewController, UniversitiesDisplayLogic {
+    var interactor: (UniversitiesDataStore & UniversitiesBusinessLogic)?
+    var router: UniversitiesRoutingLogic?
+    
+    private let tableView = UITableView()
+    private let titleLabel: UILabel = UILabel()
+    private let refreshControl: UIRefreshControl = {
+        let refreshControl = UIRefreshControl()
+        refreshControl.tintColor = .systemCyan
+        refreshControl.addTarget(self, action: #selector(handleRefresh), for: .valueChanged)
+        return refreshControl
+    }()
+    
+    private var universities: [Universities.Load.ViewModel.UniversityViewModel] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setup()
+        configureLabel()
+        configureTableView()
+        interactor?.loadUniversities(
+            Universities.Load.Request(regionID: nil, sortOption: nil, searchQuery: nil)
+        )
+    }
+    
+    func displayUniversities(viewModel: Universities.Load.ViewModel) {
+        universities = viewModel.universities
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+            self.refreshControl.endRefreshing()
+        }
+    }
+    
+    func displayError(message: String) {
+        // Покажите сообщение об ошибке
+        print("Error: \(message)")
+    }
+    
+    private func setup() {
+        let viewController = self
+        let interactor = UniversitiesInteractor()
+        let presenter = UniversitiesPresenter()
+        let router = UniversitiesRouter()
+        
+        viewController.interactor = interactor
+        viewController.router = router
+        interactor.presenter = presenter
+        presenter.viewController = viewController
+        router.viewController = viewController
+    }
+    
+    private func configureLabel() {
+        view.addSubview(titleLabel)
+        
+        titleLabel.font =  UIFont(name: "MontserratAlternates-Bold", size: 28)
+        
+        titleLabel.text = "ВУЗы"
+        titleLabel.textAlignment = .center
+        
+        titleLabel.pinTop(to: view.safeAreaLayoutGuide.topAnchor, 25)
+        titleLabel.pinLeft(to: view.leadingAnchor, 20)
     }
 }
+
+
+// MARK: - UITableViewDataSource & UITableViewDelegate
+extension UniversitiesViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    struct Constants {
+        static let universityCellIdentifier = "UniversityTableViewCell"
+        static let tableViewBackgroundColor: UIColor = .white
+        static let shareActionTitle = "Share"
+    }
+    
+    private func configureTableView() {
+        view.addSubview(tableView)
+        
+        tableView.pinTop(to: titleLabel.bottomAnchor)
+        tableView.pinLeft(to: view.leadingAnchor)
+        tableView.pinRight(to: view.trailingAnchor)
+        tableView.pinBottom(to: view.bottomAnchor)
+        
+        tableView.register(UniversityTableViewCell.self, forCellReuseIdentifier: Constants.universityCellIdentifier)
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.backgroundColor = Constants.tableViewBackgroundColor
+        tableView.separatorStyle = .singleLine
+        
+        tableView.refreshControl = refreshControl
+        tableView.showsVerticalScrollIndicator = false
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return universities.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: Constants.universityCellIdentifier,
+            for: indexPath
+        ) as! UniversityTableViewCell
+        let universityViewModel = universities[indexPath.row]
+        cell.configure(with: universityViewModel)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        guard let universityModel = interactor?.universities[indexPath.row] else { return }
+        router?.routeToDetails(for: universityModel)
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let offset = scrollView.contentOffset.y
+        
+        let scaleFactor = min(1.2, max(0.75, 1 - offset / 200))
+        
+        titleLabel.transform = CGAffineTransform(scaleX: scaleFactor, y: scaleFactor)
+        
+        let scaledWidth = titleLabel.bounds.width * (1 - scaleFactor)
+        titleLabel.transform = titleLabel.transform.translatedBy(x: -scaledWidth / 2, y: 0)
+    }
+    
+    @objc
+    private func handleRefresh() {
+        // Имитация загрузки данных
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            self.interactor?.loadUniversities(
+                Universities.Load.Request(regionID: nil, sortOption: nil, searchQuery: nil)
+            )
+            self.refreshControl.endRefreshing() // Останавливаем анимацию
+        }
+    }
+}
+
+
