971ced58f1fd3ecf7f809acbedc510df6fe5be39
sundayti <pankratovvlad1@gmail.com>
2024-12-27 23:56:33 +0300

    Update UniversitiesRouter

--- clients/iOS/olympguide/olympguide/Application/Scenes/UniversitiesScene/VIP/UniversitiesRouter.swift
+++ clients/iOS/olympguide/olympguide/Application/Scenes/UniversitiesScene/VIP/UniversitiesRouter.swift
@@ -5,4 +5,14 @@
 //  Created by Tom Tim on 22.12.2024.
 //
 
-import Foundation
+import UIKit
+
+final class UniversitiesRouter: UniversitiesRoutingLogic {
+    weak var viewController: UIViewController?
+
+    func routeToDetails(for university: UniversityModel) {
+        let detailsViewController = UIViewController() // Допустим, у вас есть экран деталей
+//        detailsViewController.university = university
+        viewController?.navigationController?.pushViewController(detailsViewController, animated: true)
+    }
+}
