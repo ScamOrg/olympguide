1bf839acb8b14263f5d1a6604ce64366335c9769
sundayti <pankratovvlad1@gmail.com>
2025-01-09 11:55:17 +0300

    Create SearchInteractor.swift

---
+++ clients/iOS/olympguide/olympguide/Application/Scenes/SearchScene/VIP/SearchInteractor.swift
@@ -0,0 +1,61 @@
+//
+//  File.swift
+//  olympguide
+//
+//  Created by Tom Tim on 01.01.2025.
+//
+
+import Foundation
+
+final class SearchInteractor: SearchBusinessLogic, SearchDataStore {
+    
+    // MARK: - VIP
+    var presenter: SearchPresentationLogic?
+    private let worker = SearchWorker()
+    
+    // MARK: - DataStore
+    var searchType: SearchType?
+    var results: [String] = []
+    
+    // MARK: - Business Logic
+    
+    func loadScene(request: Search.Load.Request) {
+        self.searchType = request.searchType
+        
+        let titleForScene: String
+        switch request.searchType {
+        case .universities:
+            titleForScene = "Поиск ВУЗов"
+        case .olympiads:
+            titleForScene = "Поиск олимпиад"
+        case .other:
+            titleForScene = "Поиск"
+        }
+        
+        // Презентуем заголовок
+        let response = Search.Load.Response(title: titleForScene)
+        presenter?.presentLoadScene(response: response)
+        
+        // 1) Сразу же делаем «пустой» запрос — чтобы отобразить дефолтный список
+        let textDidChangeRequest = Search.TextDidChange.Request(query: "")
+        textDidChange(request: textDidChangeRequest)
+    }
+    
+    // SearchInteractor.swift
+    func textDidChange(request: Search.TextDidChange.Request) {
+        guard let type = searchType else { return }
+        let allItems = worker.fetchData(for: type)
+        let filtered = worker.filter(items: allItems, with: request.query)
+        results = filtered
+        let response = Search.TextDidChange.Response(results: filtered)
+        presenter?.presentTextDidChange(response: response)
+    }
+    
+    func selectItem(request: Search.SelectItem.Request) {
+        guard request.index < results.count else { return }
+        let selected = results[request.index]
+        
+        let response = Search.SelectItem.Response(selectedItem: selected)
+        presenter?.presentSelectItem(response: response)
+    }
+}
