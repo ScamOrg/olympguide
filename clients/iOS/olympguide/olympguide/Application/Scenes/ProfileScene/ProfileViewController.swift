27e8ece21c07449039201cf3e17d1d824a16a04a
sundayti <pankratovvlad1@gmail.com>
2025-01-10 08:24:28 +0300

    Create ProfileViewController.swift

---
+++ clients/iOS/olympguide/olympguide/Application/Scenes/ProfileScene/ProfileViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ProfileViewController.swift
+//  olympguide
+//
+//  Created by Tom Tim on 10.01.2025.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
