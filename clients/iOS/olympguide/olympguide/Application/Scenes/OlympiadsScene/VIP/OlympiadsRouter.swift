125f274abc1571d2af222c228020044f21c3eedf
sundayti <pankratovvlad1@gmail.com>
2025-01-10 08:24:18 +0300

    Create OlympiadsRouter.swift

---
+++ clients/iOS/olympguide/olympguide/Application/Scenes/OlympiadsScene/VIP/OlympiadsRouter.swift
@@ -0,0 +1,24 @@
+//
+//  OlympiadsRouter.swift
+//  olympguide
+//
+//  Created by Tom Tim on 09.01.2025.
+//
+
+import UIKit
+
+final class OlympiadsRouter: OlympiadsRoutingLogic {
+    weak var viewController: UIViewController?
+
+    func routeToDetails(for university: OlympiadModel) {
+        let detailsViewController = ViewController() // Допустим, у вас есть экран деталей
+//        detailsViewController.university = university
+        viewController?.navigationController?.pushViewController(detailsViewController, animated: true)
+    }
+    
+    func routeToSearch() {
+        let searchVC = SearchViewController(searchType: .olympiads)
+        searchVC.modalPresentationStyle = .overFullScreen
+        viewController?.navigationController?.pushViewController(searchVC, animated: true)
+    }
+}
