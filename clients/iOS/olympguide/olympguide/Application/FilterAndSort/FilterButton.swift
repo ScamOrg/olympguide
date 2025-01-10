ad2da9d3fda246dcffe135ac892a20189e9a4014
sundayti <pankratovvlad1@gmail.com>
2025-01-10 08:22:00 +0300

    Update FilterButton.swift
    Add Marks and Constants

--- clients/iOS/olympguide/olympguide/Application/FilterAndSort/FilterButton.swift
+++ clients/iOS/olympguide/olympguide/Application/FilterAndSort/FilterButton.swift
@@ -7,15 +7,42 @@
 
 import UIKit
 
+// MARK: - Constants
+fileprivate enum Constants {
+    enum Images {
+        static let arrowImageName = "chevron.down"
+    }
+    
+    enum Colors {
+        static let backgroundColor = UIColor(hex: "#EBEBEC")
+        static let arrowTintColor = UIColor.black
+    }
+    
+    enum Fonts {
+        static let titleFont = UIFont(name: "MontserratAlternates-Regular", size: 14)!
+    }
+    
+    enum Dimensions {
+        static let cornerRadius: CGFloat = 15.5
+        static let buttonHeight: CGFloat = 31
+        static let titleBottomMargin: CGFloat = 2
+        static let titleLeftMargin: CGFloat = 16
+        static let titleRightMargin: CGFloat = 6
+        static let arrowRightMargin: CGFloat = 14
+    }
+}
+
 class FilterButton: UIButton {
+    
+    // MARK: - Variables
     private var title: UILabel = UILabel()
-    let arrowImageView: UIImageView = UIImageView(image: UIImage(systemName: "chevron.down"))
+    let arrowImageView: UIImageView = UIImageView(image: UIImage(systemName: Constants.Images.arrowImageName))
     
     var filterTitle: String {
         return title.text ?? ""
     }
     
-    // MARK: - Initialization
+    // MARK: - Lifecycle
     init(title: String) {
         super.init(frame: .zero)
         self.title.text = title
@@ -27,33 +54,33 @@ class FilterButton: UIButton {
         fatalError("init(coder:) has not been implemented")
     }
     
-    private func configureUI() {
-        self.addSubview(title)
-        self.addSubview(arrowImageView)
-        
-        self.backgroundColor = UIColor(hex: "#EBEBEC")
-        self.layer.cornerRadius = 15.5 // Половина высоты кнопки
-        self.clipsToBounds = true
-        self.setHeight(31)
-        
-        arrowImageView.tintColor = .black // Делаем `chevron.down` чёрной
-        arrowImageView.contentMode = .scaleAspectFit
-        
-        title.font = UIFont(name: "MontserratAlternates-Regular", size: 14)
-        title.pinBottom(to: self.bottomAnchor, 2)
-        title.pinLeft(to: self.leadingAnchor, 16)
-        title.pinTop(to: self.topAnchor)
-        title.pinRight(to: arrowImageView.leadingAnchor, 6)
-        
-        arrowImageView.pinCenterY(to: self)
-        arrowImageView.pinRight(to: self.trailingAnchor, 14)
-    }
-    
-    // MARK: - Обработка нажатий
+    // MARK: - Methods
     override var isHighlighted: Bool {
         didSet {
-            // При нажатии изменяем прозрачность для эффекта "нажатия"
             self.alpha = isHighlighted ? 0.7 : 1.0
         }
+    }
+    
+    // MARK: - Private funcs
+    private func configureUI() {
+        addSubview(title)
+        addSubview(arrowImageView)
+        
+        backgroundColor = Constants.Colors.backgroundColor
+        layer.cornerRadius = Constants.Dimensions.cornerRadius
+        clipsToBounds = true
+        setHeight(Constants.Dimensions.buttonHeight)
+        
+        arrowImageView.tintColor = Constants.Colors.arrowTintColor
+        arrowImageView.contentMode = .scaleAspectFit
+        
+        title.font = Constants.Fonts.titleFont
+        title.pinBottom(to: self.bottomAnchor, Constants.Dimensions.titleBottomMargin)
+        title.pinLeft(to: self.leadingAnchor, Constants.Dimensions.titleLeftMargin)
+        title.pinTop(to: self.topAnchor)
+        title.pinRight(to: arrowImageView.leadingAnchor, Constants.Dimensions.titleRightMargin)
+        
+        arrowImageView.pinCenterY(to: self)
+        arrowImageView.pinRight(to: self.trailingAnchor, Constants.Dimensions.arrowRightMargin)
     }
 }
