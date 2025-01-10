f88393701cd08e8cb21e52024041a54f0ed1ff63
sundayti <pankratovvlad1@gmail.com>
2025-01-10 08:22:12 +0300

    Update OptionsTableViewCell.swift
    Add Marks and Constants

--- clients/iOS/olympguide/olympguide/Application/FilterAndSort/OptionsTableViewCell.swift
+++ clients/iOS/olympguide/olympguide/Application/FilterAndSort/OptionsTableViewCell.swift
@@ -7,13 +7,43 @@
 
 import UIKit
 
+// MARK: - Constants
+fileprivate enum Constants {
+    enum Colors {
+        static let separatorColor = UIColor(hex: "#E7E7E7")
+    }
+    
+    enum Fonts {
+        static let titleFont = UIFont(name: "MontserratAlternates-Medium", size: 15)!
+    }
+    
+    enum Dimensions {
+        static let titleLeftMargin: CGFloat = 20
+        static let actionButtonRightMargin: CGFloat = 20
+        static let titleRightMargin: CGFloat = 8
+        static let actionButtonSize: CGFloat = 24
+        static let separatorHeight: CGFloat = 1
+        static let separatorLeftMargin: CGFloat = 20
+        static let separatorRightMargin: CGFloat = 20
+    }
+    
+    enum Images {
+        static let buttonImageName = "square"
+    }
+    
+    enum Strings {
+        static let identifier = "CustomTableViewCell"
+    }
+}
+
 class OptionsTableViewCell: UITableViewCell {
     
-    static let identifier = "CustomTableViewCell"
+    // MARK: - Variables
+    static let identifier = Constants.Strings.identifier
     
     let titleLabel: UILabel = {
         let label = UILabel()
-        label.font = UIFont(name: "MontserratAlternates-Medium", size: 15)
+        label.font = Constants.Fonts.titleFont
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
@@ -22,63 +52,60 @@ class OptionsTableViewCell: UITableViewCell {
         let button = UIButton(type: .system)
         button.translatesAutoresizingMaskIntoConstraints = false
         button.tintColor = .black
-        // Устанавливаем начальное изображение
-        button.setImage(UIImage(systemName: "square"), for: .normal)
+        button.setImage(UIImage(systemName: Constants.Images.buttonImageName), for: .normal)
         return button
     }()
     
-    // Кастомный сепаратор
     private let separatorLine: UIView = {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = UIColor(hex: "#E7E7E7")
+        view.backgroundColor = Constants.Colors.separatorColor
         return view
     }()
     
-    // Callback для обработки нажатия кнопки
     var buttonAction: (() -> Void)?
     
+    // MARK: - Lifecycle
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        
-        // Отключаем стандартное выделение
         selectionStyle = .none
-        
-        // Добавляем подвиды
         contentView.addSubview(titleLabel)
         contentView.addSubview(actionButton)
         contentView.addSubview(separatorLine)
-        
-        // Настраиваем ограничения
-        titleLabel.pinLeft(to: contentView.leadingAnchor, 20)
-        titleLabel.pinCenterY(to: contentView)
-        
-        actionButton.pinRight(to: contentView.trailingAnchor, 20)
-        actionButton.pinCenterY(to: contentView)
-        actionButton.setHeight(24)
-        actionButton.setWidth(24)
-        
-        titleLabel.pinRight(to: actionButton.leadingAnchor, 8, .lsOE)
-        
-        separatorLine.pinLeft(to: contentView.leadingAnchor, 20)
-        separatorLine.pinRight(to: contentView.trailingAnchor, 20)
-        separatorLine.pinBottom(to: contentView.bottomAnchor)
-        separatorLine.setHeight(1)
-        
-        // Добавляем действие на кнопку
+        setupConstraints()
         actionButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
-    }
-    
-    @objc private func buttonTapped() {
-        buttonAction?()
-    }
-    
-    // Метод для скрытия сепаратора
-    func hideSeparator(_ hide: Bool) {
-        separatorLine.isHidden = hide
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Methods
+    func hideSeparator(_ hide: Bool) {
+        separatorLine.isHidden = hide
+    }
+    
+    // MARK: - Private funcs
+    private func setupConstraints() {
+        titleLabel.pinLeft(to: contentView.leadingAnchor, Constants.Dimensions.titleLeftMargin)
+        titleLabel.pinCenterY(to: contentView)
+        
+        actionButton.pinRight(to: contentView.trailingAnchor, Constants.Dimensions.actionButtonRightMargin)
+        actionButton.pinCenterY(to: contentView)
+        actionButton.setHeight(Constants.Dimensions.actionButtonSize)
+        actionButton.setWidth(Constants.Dimensions.actionButtonSize)
+        
+        titleLabel.pinRight(to: actionButton.leadingAnchor, Constants.Dimensions.titleRightMargin, .lsOE)
+        
+        separatorLine.pinLeft(to: contentView.leadingAnchor, Constants.Dimensions.separatorLeftMargin)
+        separatorLine.pinRight(to: contentView.trailingAnchor, Constants.Dimensions.separatorRightMargin)
+        separatorLine.pinBottom(to: contentView.bottomAnchor)
+        separatorLine.setHeight(Constants.Dimensions.separatorHeight)
+    }
+    
+    // MARK: - Objc funcs
+    @objc
+    private func buttonTapped() {
+        buttonAction?()
     }
 }
