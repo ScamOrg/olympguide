a3f15f736fdccbdfc4ba045d481ee56cb2ee1537
sundayti <pankratovvlad1@gmail.com>
2025-01-11 14:49:08 +0300

    Create FieldsRouter.swift

---
+++ clients/iOS/olympguide/olympguide/Application/Scenes/FieldsScene/VIP/FieldsRouter.swift
@@ -0,0 +1,24 @@
+//
+//  FieldsRouter.swift
+//  olympguide
+//
+//  Created by Tom Tim on 22.12.2024.
+//
+
+import UIKit
+
+final class FieldsRouter: FieldsRoutingLogic {
+    weak var viewController: UIViewController?
+
+    func routeToDetails(for field: GroupOfFieldsModel.FieldModel) {
+        let detailsViewController = ViewController() // Допустим, у вас есть экран деталей
+//        detailsViewController.field = field
+        viewController?.navigationController?.pushViewController(detailsViewController, animated: true)
+    }
+    
+    func routeToSearch() {
+        let searchVC = SearchViewController(searchType: .fields)
+        searchVC.modalPresentationStyle = .overFullScreen
+        viewController?.navigationController?.pushViewController(searchVC, animated: true)
+    }
+}
