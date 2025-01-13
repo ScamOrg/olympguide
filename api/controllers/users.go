285b9414bb51295560b64381e5ba0dc46f32cd64
titarenko <arseniytitarenko93@yandex.ru>
2025-01-13 11:59:38 +0300

    add /sign_up

--- api/controllers/users.go
+++ api/controllers/users.go
@@ -3,11 +3,14 @@ package controllers
 import (
 	"api/constants"
 	"api/db"
+	"api/models"
 	"context"
 	"errors"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/go-redis/redis/v8"
+	"golang.org/x/crypto/bcrypt"
+	"log"
 	"math/rand"
 	"net/http"
 	"time"
@@ -24,6 +27,21 @@ type VerifyRequest struct {
 	Code  string `json:"code" binding:"required"`
 }
 
+type SignUpRequest struct {
+	Email      string    `json:"email" binding:"required"`
+	Password   string    `json:"password" binding:"required"`
+	FirstName  string    `json:"first_name" binding:"required"`
+	LastName   string    `json:"last_name" binding:"required"`
+	SecondName string    `json:"second_name" binding:"omitempty,min=1"`
+	Birthday   time.Time `json:"birthday" binding:"required"`
+	RegionID   string    `json:"region_id"`
+}
+
+type Message struct {
+	Email string `json:"email"`
+	Code  string `json:"code"`
+}
+
 func SendCode(c *gin.Context) {
 	var request SendRequest
 	if err := c.ShouldBindJSON(&request); err != nil {
@@ -32,14 +50,20 @@ func SendCode(c *gin.Context) {
 	}
 	code := GenerateCode()
 
-	// собственно отправка email и кода через брокер сообщений в python
-	fmt.Printf("Code %s sent to %s", code, request.Email)
-
 	err := db.Redis.Set(ctx, request.Email, code, constants.EMAIL_CODE_TTL*time.Minute).Err()
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
+
+	msg := Message{request.Email, code}
+	err = db.Redis.Publish(ctx, "email_codes", msg).Err()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	log.Printf("Code %s sent to %s", code, request.Email)
+
 	c.JSON(http.StatusOK, gin.H{"message": "Код отправлен на " + request.Email})
 }
 
@@ -62,16 +86,45 @@ func VerifyCode(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Неверный код"})
 		return
 	}
-
-	err = db.Redis.Del(ctx, request.Email).Err()
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Ошибка при удалении кода из Redis"})
-		return
-	}
+	db.Redis.Del(ctx, request.Email)
 	c.JSON(http.StatusOK, gin.H{"message": "Email успешно подтверждён"})
 }
 
 func GenerateCode() string {
 	code := fmt.Sprintf("%04d", rand.Intn(10000))
 	return code
+}
+
+func SignUp(c *gin.Context) {
+	var request SignUpRequest
+	if err := c.ShouldBindJSON(&request); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	hashedPassword, err := HashPassword(request.Password)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	}
+	user := models.User{
+		Email:        request.Email,
+		FirstName:    request.FirstName,
+		LastName:     request.LastName,
+		SecondName:   request.SecondName,
+		Birthday:     request.Birthday,
+		PasswordHash: hashedPassword,
+	}
+	result := db.DB.Create(&user)
+	if result.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": result.Error.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"user_id": user.UserID})
+}
+
+func HashPassword(password string) (string, error) {
+	hashedBytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hashedBytes), nil
 }
