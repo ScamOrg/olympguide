6822c4224da2887f61952c4b55f199e1c9a0c856
sundayti <pankratovvlad1@gmail.com>
2025-01-14 03:57:33 +0300

    Update TabBarViewController.swift
    Add New VC to test new header's logic

--- clients/iOS/olympguide/olympguide/Application/TabBar/TabBarViewController.swift
+++ clients/iOS/olympguide/olympguide/Application/TabBar/TabBarViewController.swift
@@ -240,18 +240,82 @@ class MainViewController: UIViewController {
     
     //
         private let customSearchBar = CustomSearchBar(title: "Найти")
+import UIKit
+
+class BrowseViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            view.backgroundColor = .white
+    let tableView = UITableView()
+    let refreshControl = UIRefreshControl()
     
-            // Допустим, хотим разместить на экране
-            customSearchBar.frame = CGRect(
-                x: 20,
-                y: 100,
-                width: UIScreen.main.bounds.width - 40,
-                height: 50
-            )
-            view.addSubview(customSearchBar)
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+//        navigationItem.largeTitleDisplayMode = .automatic
+
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.title = "Browse"
+        
+
+        
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search"
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
+        
+        setupTableView()
+        setupRefreshControl()
+    }
+    
+    func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.frame = view.bounds
+        tableView.refreshControl = refreshControl
+        view.addSubview(tableView)
+    }
+    
+    func setupRefreshControl() {
+        refreshControl.addTarget(self, action: #selector(refreshData), for: .valueChanged)
+        refreshControl.tintColor = .systemBlue
+    }
+    
+    @objc func refreshData() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) { [weak self] in
+            self?.refreshControl.endRefreshing()
         }
+    }
+        
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        cell.textLabel?.text = "Item \(indexPath.row + 1)"
+        cell.accessoryType = .disclosureIndicator
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let detailVC = DetailViewController()
+        detailVC.title = "Item \(indexPath.row + 1)"
+        navigationController?.pushViewController(detailVC, animated: true)
+    }
+}
+
+class DetailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        navigationItem.largeTitleDisplayMode = .never
+
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+//        navigationController?.navigationBar.prefersLargeTitles = false
+    }
 }
