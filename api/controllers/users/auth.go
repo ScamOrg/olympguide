2948ae97289e9ea72ca3f10f10e5673ca7ff3ce6
titarenko <arseniytitarenko93@yandex.ru>
2025-01-14 19:19:37 +0300

    reorganized

---
+++ api/controllers/users/auth.go
@@ -0,0 +1,120 @@
+package users
+
+import (
+	"api/db"
+	"api/models"
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
+	"log"
+	"net/http"
+	"time"
+)
+
+type SignUpRequest struct {
+	Email      string `json:"email" binding:"required"`
+	Password   string `json:"password" binding:"required"`
+	FirstName  string `json:"first_name" binding:"required"`
+	LastName   string `json:"last_name" binding:"required"`
+	SecondName string `json:"second_name" binding:"omitempty,min=1"`
+	Birthday   string `json:"birthday" binding:"required"`
+	RegionID   uint   `json:"region_id" binding:"required"`
+}
+
+type LoginRequest struct {
+	Email    string `json:"email" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
+func SignUp(c *gin.Context) {
+	var request SignUpRequest
+	if err := c.ShouldBind(&request); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	parsedBirthday, err := time.Parse("02.01.2006", request.Birthday)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid birthday format, use DD.MM.YYYY"}) // Возвращаем ошибку, если формат неверный
+		return
+	}
+
+	var regionExists bool
+	db.DB.Raw("SELECT EXISTS(SELECT 1 FROM olympguide.region WHERE region_id = ?)", request.RegionID).Scan(&regionExists)
+	if !regionExists {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Region does not exist"})
+		return
+	}
+
+	hashedPassword, err := HashPassword(request.Password)
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal error"})
+	}
+	user := models.User{
+		Email:        request.Email,
+		FirstName:    request.FirstName,
+		LastName:     request.LastName,
+		SecondName:   request.SecondName,
+		Birthday:     parsedBirthday,
+		PasswordHash: hashedPassword,
+		RegionID:     request.RegionID,
+	}
+	result := db.DB.Create(&user)
+	if result.Error != nil {
+		log.Println(err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal error"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "Signed up", "user_id": user.UserID})
+}
+
+func Login(c *gin.Context) {
+	var request LoginRequest
+	if err := c.ShouldBind(&request); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Email and password are required"})
+		return
+	}
+
+	hashedPassword, err := HashPassword(request.Password)
+	if err != nil {
+		log.Println("Can't generate password_hash")
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal error"})
+		return
+	}
+
+	var user models.User
+	if err := db.DB.Where("email = ? AND password_hash = ?", request.Email, hashedPassword).First(&user).Error; err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid credentials"})
+		return
+	}
+
+	session := sessions.Default(c)
+	session.Set("user_id", user.UserID)
+	if err := session.Save(); err != nil {
+		log.Println(err.Error())
+		c.JSON(http.StatusInternalServerError, gin.H{"message": "Error"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Logged in", "user_id": user.UserID})
+}
+
+func Logout(c *gin.Context) {
+	session := sessions.Default(c)
+	session.Clear()
+	err := session.Save()
+	if err != nil {
+		log.Println(err.Error())
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "error"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "Logged out"})
+}
+
+func HashPassword(password string) (string, error) {
+	hashedBytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hashedBytes), nil
+}
---
+++ api/controllers/users/code.go
@@ -0,0 +1,89 @@
+package users
+
+import (
+	"api/constants"
+	"api/db"
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v8"
+	"log"
+	"math/rand"
+	"net/http"
+	"time"
+)
+
+var ctx = context.Background()
+
+type SendRequest struct {
+	Email string `json:"email" binding:"required"`
+}
+
+type VerifyRequest struct {
+	Email string `json:"email" binding:"required"`
+	Code  string `json:"code" binding:"required"`
+}
+
+type Message struct {
+	Email string `json:"email"`
+	Code  string `json:"code"`
+}
+
+func SendCode(c *gin.Context) {
+	var request SendRequest
+	if err := c.ShouldBind(&request); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	code := GenerateCode()
+
+	err := db.Redis.Set(ctx, request.Email, code, constants.EMAIL_CODE_TTL*time.Minute).Err()
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal error"})
+		return
+	}
+
+	msg := Message{request.Email, code}
+	msgJSON, _ := json.Marshal(msg)
+	err = db.Redis.Publish(ctx, "email_codes", msgJSON).Err()
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal error"})
+		return
+	}
+	log.Printf("Code %s sent to %s", code, request.Email)
+
+	c.JSON(http.StatusOK, gin.H{"message": "Code sent to " + request.Email})
+}
+
+func VerifyCode(c *gin.Context) {
+	var request VerifyRequest
+	if err := c.ShouldBind(&request); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	}
+
+	storedCode, err := db.Redis.Get(ctx, request.Email).Result()
+	if errors.Is(err, redis.Nil) {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Code was not found or expired"})
+		return
+	} else if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal error"})
+		return
+	}
+
+	if storedCode != request.Code {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid code"})
+		return
+	}
+	db.Redis.Del(ctx, request.Email)
+	c.JSON(http.StatusOK, gin.H{"message": "Email confirmed"})
+}
+
+func GenerateCode() string {
+	code := fmt.Sprintf("%04d", rand.Intn(10000))
+	return code
+}
