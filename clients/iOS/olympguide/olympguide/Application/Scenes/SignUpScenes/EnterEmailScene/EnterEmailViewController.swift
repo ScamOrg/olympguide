c73cc89224ee4bada786b93d9959e80a19656f54
sundayti <pankratovvlad1@gmail.com>
2025-01-21 00:51:33 +0300

    Update EnterEmailViewController.swift

--- clients/iOS/olympguide/olympguide/Application/Scenes/SignUpScenes/EnterEmailScene/EnterEmailViewController.swift
+++ clients/iOS/olympguide/olympguide/Application/Scenes/SignUpScenes/EnterEmailScene/EnterEmailViewController.swift
@@ -16,7 +16,7 @@ final class EnterEmailViewController: UIViewController {
         super.viewDidLoad()
         title = "Регистрация"
         configureUI()
-//        hidesBottomBarWhenPushed = true
+        //        hidesBottomBarWhenPushed = true
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -24,7 +24,7 @@ final class EnterEmailViewController: UIViewController {
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
-
+    
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
@@ -40,8 +40,6 @@ final class EnterEmailViewController: UIViewController {
     private func configureEmailTextField() {
         view.addSubview(emailTextField)
         
-        emailTextField.setHeight(48)
-        emailTextField.setWidth(UIScreen.main.bounds.width - 2 * 20)
         emailTextField.pinTop(to: view.safeAreaLayoutGuide.topAnchor, 16)
         emailTextField.pinLeft(to: view.leadingAnchor, 20)
         emailTextField.setTextFieldType(.emailAddress, .emailAddress)
@@ -52,27 +50,22 @@ final class EnterEmailViewController: UIViewController {
         nextButton.titleLabel?.tintColor = .black
         nextButton.backgroundColor = UIColor(hex: "#E0E8FE")
         nextButton.setTitle("Продолжить", for: .normal)
-
         
-//        view.addSubview(nextButton)
-//        nextButton.setHeight(48)
-//        nextButton.pinCenterX(to: view)
-//        nextButton.pinLeft(to: view.leadingAnchor, 20)
-//        nextButton.pinRight(to: view.trailingAnchor, 20)
-//        nextButton.pinBottom(to: view.bottomAnchor, 100)
+        
+        
         
         nextButton.translatesAutoresizingMaskIntoConstraints = false
-            view.addSubview(nextButton)
-            
-            NSLayoutConstraint.activate([
-                nextButton.heightAnchor.constraint(equalToConstant: 48),
-                nextButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-                nextButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
-            ])
-            
-            // Создаем и активируем нижнее ограничение, сохраняя его ссылку
-            nextButtonBottomConstraint = nextButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -43)
-            nextButtonBottomConstraint.isActive = true
+        view.addSubview(nextButton)
+        
+        NSLayoutConstraint.activate([
+            nextButton.heightAnchor.constraint(equalToConstant: 48),
+            nextButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            nextButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+        ])
+        
+        // Создаем и активируем нижнее ограничение, сохраняя его ссылку
+        nextButtonBottomConstraint = nextButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -43)
+        nextButtonBottomConstraint.isActive = true
     }
     
     @objc private func keyboardWillShow(notification: Notification) {
@@ -91,7 +84,7 @@ final class EnterEmailViewController: UIViewController {
             }
         }
     }
-
+    
     @objc private func keyboardWillHide(notification: Notification) {
         if let animationDuration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double {
             nextButtonBottomConstraint.constant = -43
