9a99950df673f18f4bb304689adb19d2f1c532ef
sundayti <pankratovvlad1@gmail.com>
2025-01-22 02:19:19 +0300

    Update EnterEmailViewController.swift

--- clients/iOS/olympguide/olympguide/Application/Scenes/SignUpScenes/EnterEmailScene/EnterEmailViewController.swift
+++ clients/iOS/olympguide/olympguide/Application/Scenes/SignUpScenes/EnterEmailScene/EnterEmailViewController.swift
@@ -8,29 +8,55 @@
 import UIKit
 
 final class EnterEmailViewController: UIViewController {
+    
+    // MARK: - VIP
+    var interactor: EnterEmailBusinessLogic?
+    var router: (EnterEmailRoutingLogic & EnterEmailDataPassing)?
+    
+    // MARK: - UI
     private let emailTextField = CustomSearchBar(title: "email")
     private let nextButton: UIButton = UIButton(type: .system)
     private var nextButtonBottomConstraint: NSLayoutConstraint!
+    private var currentEmail: String = ""
     
+    // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Регистрация"
+        configure()
         configureUI()
-        //        hidesBottomBarWhenPushed = true
+        let backItem = UIBarButtonItem(title: "Регистрация", style: .plain, target: nil, action: nil)
+        navigationItem.backBarButtonItem = backItem
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        subscribeToKeyboardNotifications()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+        unsubscribeFromKeyboardNotifications()
     }
     
+    private func configure() {
+        let viewController = self
+        let interactor = EnterEmailInteractor()
+        let presenter = EnterEmailPresenter()
+        let router = EnterEmailRouter()
+        
+        viewController.interactor = interactor
+        viewController.router = router
+        
+        interactor.presenter = presenter
+        
+        presenter.viewController = viewController
+        
+        router.viewController = viewController
+        router.dataStore = interactor
+    }
+    
+    // MARK: - Private UI Setup
     private func configureUI() {
         view.backgroundColor = .white
         configureEmailTextField()
@@ -43,16 +69,15 @@ final class EnterEmailViewController: UIViewController {
         emailTextField.pinTop(to: view.safeAreaLayoutGuide.topAnchor, 16)
         emailTextField.pinLeft(to: view.leadingAnchor, 20)
         emailTextField.setTextFieldType(.emailAddress, .emailAddress)
+        emailTextField.delegate = self
     }
+    
     private func configureNextButton() {
         nextButton.titleLabel?.font = UIFont(name: "MontserratAlternates-Medium", size: 15)!
         nextButton.layer.cornerRadius = 13
         nextButton.titleLabel?.tintColor = .black
         nextButton.backgroundColor = UIColor(hex: "#E0E8FE")
         nextButton.setTitle("Продолжить", for: .normal)
-        
-        
-        
         
         nextButton.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(nextButton)
@@ -63,20 +88,61 @@ final class EnterEmailViewController: UIViewController {
             nextButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
         ])
         
-        // Создаем и активируем нижнее ограничение, сохраняя его ссылку
         nextButtonBottomConstraint = nextButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -43)
         nextButtonBottomConstraint.isActive = true
+        
+        nextButton.addTarget(self, action: #selector(didTapNextButton), for: .touchUpInside)
     }
     
-    @objc private func keyboardWillShow(notification: Notification) {
+    // MARK: - Actions
+    @objc private func didTapNextButton() {
+        let request = EnterEmailModels.SendCode.Request(email: currentEmail)
+        interactor?.sendCode(request: request)
+    }
+}
+
+extension EnterEmailViewController: CustomSearchBarDelegate {
+    func customSearchBar(_ searchBar: CustomSearchBar, textDidChange text: String) {
+        currentEmail = text
+    }
+}
+
+// MARK: - Keyboard handling
+extension EnterEmailViewController {
+    private func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillShow(notification:)),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil
+        )
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillHide(notification:)),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil
+        )
+    }
+    
+    private func unsubscribeFromKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(
+            self,
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil
+        )
+        
+        NotificationCenter.default.removeObserver(
+            self,
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil
+        )
+    }
+    
+    @objc
+    private func keyboardWillShow(notification: Notification) {
         if let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect,
            let animationDuration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double {
-            
-            // Получаем высоту клавиатуры
             let keyboardHeight = keyboardFrame.height
-            
-            // Обновляем константу нижнего ограничения кнопки, чтобы она поднялась над клавиатурой
-            // Здесь можно добавить отступ, если нужно
             nextButtonBottomConstraint.constant = -keyboardHeight - 10
             
             UIView.animate(withDuration: animationDuration) {
@@ -85,13 +151,27 @@ final class EnterEmailViewController: UIViewController {
         }
     }
     
-    @objc private func keyboardWillHide(notification: Notification) {
+    @objc
+    private func keyboardWillHide(notification: Notification) {
         if let animationDuration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double {
             nextButtonBottomConstraint.constant = -43
             
             UIView.animate(withDuration: animationDuration) {
                 self.view.layoutIfNeeded()
             }
+        }
+    }
+}
+
+// MARK: - EnterEmailDisplayLogic
+extension EnterEmailViewController: EnterEmailDisplayLogic {
+    func displaySendCodeResult(viewModel: EnterEmailModels.SendCode.ViewModel) {
+        if let errorMessage = viewModel.errorMessage {
+            let alert = UIAlertController(title: "Ошибка", message: errorMessage, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default))
+            present(alert, animated: true)
+        } else {
+            router?.routeToInputCode()
         }
     }
 }
