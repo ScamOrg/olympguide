ad0fde9082c3da4ba79923f28198be66ea02bed3
sundayti <pankratovvlad1@gmail.com>
2025-01-22 02:19:34 +0300

    Create EnterEmailWorker.swift

---
+++ clients/iOS/olympguide/olympguide/Application/Scenes/SignUpScenes/EnterEmailScene/VIP/EnterEmailWorker.swift
@@ -0,0 +1,47 @@
+//
+//  EnterEmailWorker.swift
+//  olympguide
+//
+//  Created by Tom Tim on 21.01.2025.
+//
+
+import Foundation
+
+protocol EnterEmailWorkerLogic {
+    func sendCode(email: String, completion: @escaping (Error?) -> Void)
+}
+
+final class EnterEmailWorker: EnterEmailWorkerLogic {
+    
+    func sendCode(email: String, completion: @escaping (Error?) -> Void) {
+        guard let url = URL(string: "http://5.34.212.145:8080/auth/send_code") else {
+            DispatchQueue.main.async {
+                completion(NSError(domain: "InvalidURL", code: 0))
+            }
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let jsonBody: [String: Any] = ["email": email]
+        do {
+            let data = try JSONSerialization.data(withJSONObject: jsonBody, options: [])
+            request.httpBody = data
+        } catch {
+            DispatchQueue.main.async {
+                completion(error)
+            }
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            DispatchQueue.main.async {
+                completion(error)
+            }
+        }
+        task.resume()
+    }
+}
