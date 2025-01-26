c06fb3b04c5c68ab5e8ad09f2df6ded5b97a37e8
sundayti <pankratovvlad1@gmail.com>
2025-01-26 21:49:49 +0300

    Create UIViewController+Alert.swift

---
+++ clients/iOS/olympguide/olympguide/Application/Extensions/UIViewController+Alert.swift
@@ -0,0 +1,20 @@
+//
+//  UIViewController+Alert.swift
+//  olympguide
+//
+//  Created by Tom Tim on 26.01.2025.
+//
+
+import UIKit
+
+// MARK: - UIViewController Extension
+extension UIViewController {
+
+    // MARK: - Show Alert Method
+    func showAlert(title: String = "Ошибка", message: String?, cancelTitle: String = "Отмена") {
+        let alert = UIAlertController(title: title, message: (message ?? "Ошибка"), preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: cancelTitle, style: .cancel, handler: nil)
+        alert.addAction(cancelAction)
+        present(alert, animated: true, completion: nil)
+    }
+}
