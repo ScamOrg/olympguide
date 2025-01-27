afe635bff1dc5085bb4cd4daeaa306f66572a4bf
sundayti <pankratovvlad1@gmail.com>
2025-01-28 00:15:37 +0300

    Update EnterEmailRouter.swift

--- clients/iOS/olympguide/olympguide/Application/Scenes/SignUpScenes/EnterEmailScene/VIP/EnterEmailRouter.swift
+++ clients/iOS/olympguide/olympguide/Application/Scenes/SignUpScenes/EnterEmailScene/VIP/EnterEmailRouter.swift
@@ -11,12 +11,14 @@ final class EnterEmailRouter: EnterEmailRoutingLogic, EnterEmailDataPassing {
     
     weak var viewController: UIViewController?
     var dataStore: EnterEmailDataStore?
-    
+
     func routeToInputCode() {
         // Предположим, у нас есть класс InputCodeViewController
         // (можете создать по аналогии свой VIP-модуль или обычный контроллер)
         let email = dataStore?.email
-        let inputCodeVC = VerifyEmailViewController(email: email ?? "")
+        let time = dataStore?.time
+        
+        let inputCodeVC = VerifyEmailViewController(email: email ?? "", time: time ?? 180)
         
         // Если нужно передать email в следующий экран:
         // inputCodeVC.email = dataStore?.email
