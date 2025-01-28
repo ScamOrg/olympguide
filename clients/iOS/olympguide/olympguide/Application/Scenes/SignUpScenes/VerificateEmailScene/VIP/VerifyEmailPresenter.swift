aeb5997a9ec057d1b956e82b668569ae563d963b
sundayti <pankratovvlad1@gmail.com>
2025-01-28 13:52:33 +0300

    Update VerifyEmailPresenter.swift

--- clients/iOS/olympguide/olympguide/Application/Scenes/SignUpScenes/VerificateEmailScene/VIP/VerifyEmailPresenter.swift
+++ clients/iOS/olympguide/olympguide/Application/Scenes/SignUpScenes/VerificateEmailScene/VIP/VerifyEmailPresenter.swift
@@ -6,10 +6,11 @@
 //
 
 import Foundation
+import UIKit
 
 final class VerifyEmailPresenter: VerifyEmailPresentationLogic {
     
-    weak var viewController: VerifyEmailDisplayLogic?
+    weak var viewController: (UIViewController & VerifyEmailDisplayLogic)?
     
     func presentVerifyCode(response: VerifyEmailModels.VerifyCode.Response) {
         if response.success {
@@ -28,6 +29,24 @@ final class VerifyEmailPresenter: VerifyEmailPresentationLogic {
             
             let viewModel = VerifyEmailModels.VerifyCode.ViewModel(errorMessage: errorMessage)
             viewController?.displayVerifyCodeResult(viewModel: viewModel)
+        }
+    }
+    
+    func presentResendCode(response: VerifyEmailModels.ResendCode.Response) {
+        if response.success {
+            let viewModel = VerifyEmailModels.ResendCode.ViewModel(errorMessage: nil)
+            viewController?.displayResendCodeResult(viewModel: viewModel)
+        } else {
+            let errorMessage: String
+            
+            if let error = response.error {
+                errorMessage = error.localizedDescription  
+            } else {
+                errorMessage = "Произошла неизвестная ошибка"
+            }
+            
+            let viewModel = EnterEmailModels.SendCode.ViewModel(errorMessage: errorMessage)
+            viewController?.showAlert(with: errorMessage)
         }
     }
 }
