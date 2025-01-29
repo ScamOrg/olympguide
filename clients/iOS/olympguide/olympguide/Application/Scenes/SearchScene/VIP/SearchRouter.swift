28cec770c095928fc33d139590aaccd6db6c7679
sundayti <pankratovvlad1@gmail.com>
2025-01-30 00:46:59 +0300

    Update SearchRouter.swift

--- clients/iOS/olympguide/olympguide/Application/Scenes/SearchScene/VIP/SearchRouter.swift
+++ clients/iOS/olympguide/olympguide/Application/Scenes/SearchScene/VIP/SearchRouter.swift
@@ -13,18 +13,7 @@ final class SearchRouter: SearchRoutingLogic, SearchDataPassing {
     weak var viewController: UIViewController?
     var dataStore: SearchDataStore?
     
-    /// Куда перейти после выбора результата
     func routeToSomeNextScene(selected: String) {
-        // Например, вы могли бы:
-        // 1. Передать selected в DetailsScene
-        // 2. Открыть новый экран
-        // 3. Или просто закрыть текущий экран
-        // Пример — закрыть экран:
-        
-        // Если это navigationController:
         viewController?.navigationController?.popViewController(animated: true)
-        
-        // Или dismiss (если презентовали модально)
-        // viewController?.dismiss(animated: true)
     }
 }
