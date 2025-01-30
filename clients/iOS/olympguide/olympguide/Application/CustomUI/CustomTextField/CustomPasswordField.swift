00681c7d89773ed17f82b615c85c079de23a2533
sundayti <pankratovvlad1@gmail.com>
2025-01-30 15:28:26 +0300

    Update CustomPasswordField.swift

--- clients/iOS/olympguide/olympguide/Application/CustomUI/CustomTextField/CustomPasswordField.swift
+++ clients/iOS/olympguide/olympguide/Application/CustomUI/CustomTextField/CustomPasswordField.swift
@@ -11,9 +11,37 @@ class CustomPasswordField: CustomTextField {
     override init(with title: String) {
         super.init(with: title)
         setSecureTextEntry(true)
+        setActionButtonImage(UIImage(systemName: "eye"))
+        setActionButtonTarget(
+            self,
+            #selector(showPassword),
+            for: .touchDown
+        )
+        addActionButtonTarget(
+            self,
+            #selector(hidePassword),
+            for: [
+                .touchUpInside,
+                .touchUpOutside,
+                .touchCancel
+            ]
+        )
     }
     
     @MainActor required init?(coder: NSCoder) {
         super.init(coder: coder)
+        setSecureTextEntry(true)
+        setActionButtonImage(UIImage(systemName: "eye"))
+        setActionButtonTarget(self, #selector(showPassword), for: .touchDown)
+    }
+    
+    @objc private func hidePassword() {
+        setActionButtonImage(UIImage(systemName: "eye"))
+        setSecureTextEntry(true)
+    }
+    
+    @objc private func showPassword() {
+        setActionButtonImage(UIImage(systemName: "eye.slash"))
+        setSecureTextEntry(false)
     }
 }
