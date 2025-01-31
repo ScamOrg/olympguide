4e4cadb9ff0a0107c47fc3f466e0b1b2e1e691f7
sundayti <pankratovvlad1@gmail.com>
2025-02-01 00:50:23 +0300

    Update OptionModels.swift

--- clients/iOS/olympguide/olympguide/Application/FilterAndSort/VIP/OptionModels.swift
+++ clients/iOS/olympguide/olympguide/Application/FilterAndSort/VIP/OptionModels.swift
@@ -5,7 +5,28 @@
 //  Created by Tom Tim on 31.01.2025.
 //
 
-struct OptionModel {
+enum Options {
+    enum TextDidChange {
+        struct Request {
+            let query: String
+        }
+        struct Response {
+            let options: [OptionModel]
+        }
+        struct ViewModel {
+            let options: [OptionModel]
+        }
+    }
+}
+
+class OptionModel {
     let title: String
-    let index: Int
+    let realIndex: Int
+    var currentIndex: Int
+    
+    init(title: String, realIndex: Int, currentIndex: Int) {
+        self.title = title
+        self.realIndex = realIndex
+        self.currentIndex = currentIndex
+    }
 }
