ac41078cafa0f45237e3b66e3c0afcbd4d0d0653
sundayti <pankratovvlad1@gmail.com>
2025-02-06 00:58:30 +0300

    Create PersonalDataInteractor.swift

---
+++ clients/iOS/olympguide/olympguide/Application/Scenes/SignUpScenes/PersonalDataScene/VIP/PersonalDataInteractor.swift
@@ -0,0 +1,37 @@
+//
+//  PersonalDataInteractor.swift
+//  olympguide
+//
+//  Created by Tom Tim on 05.02.2025.
+//
+
+// MARK: - Business Logic
+protocol PersonalDataBusinessLogic {
+    func sendCode(request: EnterEmailModels.SendCode.Request)
+}
+
+// MARK: - Data Store
+protocol PersonalDataEmailDataStore {
+    var email: String? { get set }
+    var time: Int? { get set }
+}
+
+// MARK: - Presentation Logic
+protocol PersonalDataPresentationLogic {
+    func presentSendCode(response: EnterEmailModels.SendCode.Response)
+}
+
+// MARK: - Display Logic
+protocol PersonalDataDisplayLogic: AnyObject {
+    func displaySendCodeResult(viewModel: EnterEmailModels.SendCode.ViewModel)
+}
+
+// MARK: - Routing Logic
+protocol PersonalDataRoutingLogic {
+    func routeToVerifyCode()
+}
+
+// MARK: - Data Passing
+protocol PersonalDataDataPassing {
+    var dataStore: EnterEmailDataStore? { get }
+}
