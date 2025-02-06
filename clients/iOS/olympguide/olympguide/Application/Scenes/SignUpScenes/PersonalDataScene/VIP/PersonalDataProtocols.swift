539d779e96b8f285ea6880852ff65aa13cb7d6b7
sundayti <pankratovvlad1@gmail.com>
2025-02-07 00:01:24 +0300

    Update PersonalDataProtocols.swift

--- clients/iOS/olympguide/olympguide/Application/Scenes/SignUpScenes/PersonalDataScene/VIP/PersonalDataProtocols.swift
+++ clients/iOS/olympguide/olympguide/Application/Scenes/SignUpScenes/PersonalDataScene/VIP/PersonalDataProtocols.swift
@@ -5,4 +5,34 @@
 //  Created by Tom Tim on 05.02.2025.
 //
 
-import Foundation
+// MARK: - Business Logic
+protocol PersonalDataBusinessLogic {
+    func signUp(request: EnterEmailModels.SendCode.Request)
+    func loadRegions()
+}
+
+// MARK: - Data Store
+protocol PersonalDataEmailDataStore {
+    var email: String? { get set }
+    var time: Int? { get set }
+}
+
+// MARK: - Presentation Logic
+protocol PersonalDataPresentationLogic {
+    func presentSendCode(response: EnterEmailModels.SendCode.Response)
+}
+
+// MARK: - Display Logic
+protocol PersonalDataDisplayLogic: AnyObject {
+    func displaySendCodeResult(viewModel: EnterEmailModels.SendCode.ViewModel)
+}
+
+// MARK: - Routing Logic
+protocol PersonalDataRoutingLogic {
+    func routeToVerifyCode()
+}
+
+// MARK: - Data Passing
+protocol PersonalDataDataPassing {
+    var dataStore: EnterEmailDataStore? { get }
+}
