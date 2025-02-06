28c45ff9c50a00daa74cbbbcadf03be863a2d32a
sundayti <pankratovvlad1@gmail.com>
2025-02-07 00:01:20 +0300

    Update PersonalDataInteractor.swift

--- clients/iOS/olympguide/olympguide/Application/Scenes/SignUpScenes/PersonalDataScene/VIP/PersonalDataInteractor.swift
+++ clients/iOS/olympguide/olympguide/Application/Scenes/SignUpScenes/PersonalDataScene/VIP/PersonalDataInteractor.swift
@@ -5,33 +5,4 @@
 //  Created by Tom Tim on 05.02.2025.
 //
 
-// MARK: - Business Logic
-protocol PersonalDataBusinessLogic {
-    func sendCode(request: EnterEmailModels.SendCode.Request)
-}
 
-// MARK: - Data Store
-protocol PersonalDataEmailDataStore {
-    var email: String? { get set }
-    var time: Int? { get set }
-}
-
-// MARK: - Presentation Logic
-protocol PersonalDataPresentationLogic {
-    func presentSendCode(response: EnterEmailModels.SendCode.Response)
-}
-
-// MARK: - Display Logic
-protocol PersonalDataDisplayLogic: AnyObject {
-    func displaySendCodeResult(viewModel: EnterEmailModels.SendCode.ViewModel)
-}
-
-// MARK: - Routing Logic
-protocol PersonalDataRoutingLogic {
-    func routeToVerifyCode()
-}
-
-// MARK: - Data Passing
-protocol PersonalDataDataPassing {
-    var dataStore: EnterEmailDataStore? { get }
-}
