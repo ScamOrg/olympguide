baebefd8bd8efc78a89e756e7447ca58ac30fead
sundayti <pankratovvlad1@gmail.com>
2025-02-08 23:53:49 +0300

    Update PersonalDataInteractor.swift

--- clients/iOS/olympguide/olympguide/Application/Scenes/SignUpScenes/PersonalDataScene/VIP/PersonalDataInteractor.swift
+++ clients/iOS/olympguide/olympguide/Application/Scenes/SignUpScenes/PersonalDataScene/VIP/PersonalDataInteractor.swift
@@ -5,4 +5,59 @@
 //  Created by Tom Tim on 05.02.2025.
 //
 
+import Foundation
 
+final class PersonalDataInteractor : PersonalDataBusinessLogic {
+    var presenter: PersonalDataPresentationLogic?
+    private let worker = PersonalDataWorker()
+    
+    func signUp(request: PersonalData.SignUp.Request) {
+        guard
+            let email = request.email?.trimmingCharacters(in: .whitespacesAndNewlines), !email.isEmpty,
+            let password = request.password?.trimmingCharacters(in: .whitespacesAndNewlines), !password.isEmpty,
+            let firstName = request.firstName?.trimmingCharacters(in: .whitespacesAndNewlines), !firstName.isEmpty,
+            let lastName = request.lastName?.trimmingCharacters(in: .whitespacesAndNewlines), !lastName.isEmpty,
+            let birthday = request.birthday?.trimmingCharacters(in: .whitespacesAndNewlines), !birthday.isEmpty,
+            let regionId = request.regionId
+        else {
+            presenter?.presentError(message: "")
+            return
+        }
+        
+        let secondName = request.secondName ?? ""
+        
+        guard request.secondName == nil || !secondName.isEmpty else {
+            presenter?.presentError(message: "")
+            return
+        }
+        
+        worker.signUp(
+            email: email,
+            password: password,
+            firstName: firstName,
+            lastName: lastName,
+            secondName: secondName,
+            birthday: birthday,
+            regionId: regionId
+        )
+        { [weak self] result in
+            guard let self = self else { return }
+            
+            switch result {
+            case .success(_):
+                let response = PersonalData.SignUp.Response(
+                    success: true,
+                    error: nil
+                )
+                self.presenter?.presentSignUp(response: response)
+                
+            case .failure(let networkError):
+                let response = PersonalData.SignUp.Response(
+                    success: false,
+                    error: networkError as NSError
+                )
+                self.presenter?.presentSignUp(response: response)
+            }
+        }
+    }
+}
