d34554130b6a00cebd045a93d45530682b7619ca
sundayti <pankratovvlad1@gmail.com>
2025-02-12 23:59:29 +0300

    Update PersonalDataPresenter.swift

--- clients/iOS/olympguide/olympguide/Application/Scenes/SignUpScenes/PersonalDataScene/VIP/PersonalDataPresenter.swift
+++ clients/iOS/olympguide/olympguide/Application/Scenes/SignUpScenes/PersonalDataScene/VIP/PersonalDataPresenter.swift
@@ -5,4 +5,17 @@
 //  Created by Tom Tim on 05.02.2025.
 //
 
-import Foundation
+final class PersonalDataPresenter : PersonalDataPresentationLogic {
+    
+    weak var viewController: PersonalDataDisplayLogic?
+    
+    func presentSignUp(response: PersonalData.SignUp.Response) {
+        let viewModel = PersonalData.SignUp.ViewModel(errorMessage: nil)
+        viewController?.displaySignUp(viewModel: viewModel)
+    }
+    
+    func presentError(with error: any Error) {
+        let viewModel = PersonalData.SignUp.ViewModel(errorMessage: error.localizedDescription)
+        viewController?.displaySignUp(viewModel: viewModel)
+    }
+}
