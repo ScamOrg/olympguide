1a95a060623ba3138364f2806450a1591d8a9bbb
sundayti <pankratovvlad1@gmail.com>
2025-02-14 20:09:28 +0300

    Update educational_programs-parser.py
    Add directions parse

--- load_data_service/hse_loader/educational_programs-parser.py
+++ load_data_service/hse_loader/educational_programs-parser.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
 
 class EducationalProgram:
@@ -10,6 +11,7 @@ class EducationalProgram:
         self.paid_places = paid_places
         self.link = link
         self.price = None
+        self.direction = None
 
     def __repr__(self):
         return (f"EducationalProgram(name='{self.name}', budget_places={self.budget_places}, "
@@ -18,7 +20,9 @@ class EducationalProgram:
     def __str__(self):
         return (f"Программа: {self.name}\nФакультеты: {', '.join(self.faculties)}\n"
                 f"Бюджетные места: {self.budget_places}\nПлатные места: {self.paid_places}\n"
-                f"Ссылка: {self.link}\n")
+                f"Ссылка: {self.link}\n"
+                f"Цена: {self.price}\nНаправление: {self.direction}\n")
+
 
 def parse_educational_programs_with_faculties(url: str) -> dict:
     response = requests.get(url)
@@ -72,12 +76,14 @@ def parse_educational_programs_with_faculties(url: str) -> dict:
 
     return data
 
+
 def parse_educational_programs_price(url):
     response = requests.get(url)
     response.encoding = 'utf-8'
 
     soup = BeautifulSoup(response.text, 'html.parser')
-    data = {}
+    prices = {}
+    directions = {}
 
     table = soup.find('table', class_='bordered data_small fixed_header').find_all('tbody')
 
@@ -88,7 +94,7 @@ def parse_educational_programs_price(url):
 
         for tr in current_trs:
             trs.append(tr)
-
+    current_direction = ""
     for tr in trs:
         tds = tr.find_all('td')
         link = tds[0].find('a')
@@ -98,57 +104,75 @@ def parse_educational_programs_price(url):
                 name = "Совместный бакалавриат НИУ ВШЭ и ЦПМ"
                 "Экономика и статистика"
             price = int(tds[1].get_text().strip()) * 1000
-            data[name] = price
+            prices[name] = price
+            directions[name] = current_direction
+        else:
+            current_direction = extract_version(tds[0].get_text(), current_direction)
 
-    return data
+    return prices, directions
+
+
+def extract_version(string, current_version):
+    match = re.search(r'\b\d{2}\.\d{2}\.\d{2}\b', string)
+    return match.group(0) if match else current_version
+
 
 def parse_educational_programs_prices(url):
     response = requests.get(url)
     response.encoding = 'utf-8'
 
     soup = BeautifulSoup(response.text, 'html.parser')
-    data = {}
-
+    prices = {}
+    directions = {}
     table = soup.find('table').find('tbody')
 
     trs = table.find_all('tr')
-
+    current_direction = ""
     for tr in trs:
         tds = tr.find_all('td')
         link = tds[0].find('a')
+
         if link:
             name = tds[0].get_text().replace('\xa0', ' ').replace('  ', ' ').split('/')[0].strip()
             if name == "Турция и тюрский мир":
                 name = "Турция и тюркский мир"
-            price = int(tds[-1].get_text().strip())
-            data[name] = price
+            price = int(tds[-1].get_text().strip()) * 1000
+            prices[name] = price
+            directions[name] = current_direction
+        else:
+            current_direction = extract_version(tds[0].get_text(), current_direction)
 
-    return data
+    return prices, directions
+
 
 if __name__ == '__main__':
     url = 'https://www.hse.ru/education/msk/bachelor/'
     educational_programs = parse_educational_programs_with_faculties(url)
     url = 'https://ba.hse.ru/price'
-    programs_price = parse_educational_programs_price(url)
+    programs_price1, programs_directions1 = parse_educational_programs_price(url)
     url = 'https://admissions.hse.ru/undergraduate-apply/fees'
-    program_prices = parse_educational_programs_prices(url)
+    programs_price2, programs_directions2 = parse_educational_programs_prices(url)
 
-    for program, price in programs_price.items():
-        if program not in educational_programs:
-            print(program, price)
-        else:
+    for program, price in programs_price1.items():
+        if program in educational_programs:
             educational_programs[program].price = price
 
-    print()
-    for program, price in program_prices.items():
-        if program not in educational_programs:
-            print(program, price)
-        else:
-            educational_programs[program].price = price
+    for program, direction in programs_directions1.items():
+        if program in educational_programs:
+            educational_programs[program].direction = direction
 
     print()
+    for program, price in programs_price2.items():
+        if program in educational_programs:
+            educational_programs[program].price = price
+
+    for program, direction in programs_directions2.items():
+        if program in educational_programs:
+            educational_programs[program].direction = direction
+    print()
+    for key in list(educational_programs.keys()):
+        if educational_programs[key].price is None or educational_programs[key].direction is None :
+            del educational_programs[key]
+
     for program, value in educational_programs.items():
-        if value.price == None:
-            print(value.name)
-
-
+        print(value)
