aaca35937b4d88cb4618621e5e705f254d862365
sundayti <pankratovvlad1@gmail.com>
2025-02-17 03:02:44 +0300

    Create main.py
    Create main in hse loader

---
+++ load_data_service/hse_loader/educational_programs_loader/main.py
@@ -0,0 +1,16 @@
+from program_loader import parse_educational_programs_with_faculties
+from subjects_loader import load_subjects_places_fields
+from costs_loader import load_costs_from_first_source, load_costs_from_second_source
+from logging_config.setup_logging import setup_logging
+import logging
+
+setup_logging()
+
+logger = logging.getLogger(__name__)
+
+def main():
+    educational_programs = parse_educational_programs_with_faculties()
+    load_subjects_places_fields(educational_programs)
+
+if __name__ == '__main__':
+    main()
