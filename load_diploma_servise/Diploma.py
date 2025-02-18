2a777b854445413753f7595a2fcb5050879ffca3
sundayti <pankratovvlad1@gmail.com>
2025-02-18 19:31:59 +0300

    Create Diploma.py

---
+++ load_diploma_servise/Diploma.py
@@ -0,0 +1,6 @@
+class Diploma:
+    def __init__(self, user_id, olympiad_id, diploma_class, level):
+        self.user_id = user_id
+        self.olympiad_id = olympiad_id
+        self.diploma_class = diploma_class
+        self.level = level
