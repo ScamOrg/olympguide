9441b076830d8312ddaf39959758b0faf6bc629c
sundayti <pankratovvlad1@gmail.com>
2025-02-20 00:45:35 +0300

    Update ProfileTableViewCell.swift
    Rebuild ProfileTableViewCell because there were errors with Layout in Reusable Cell

--- clients/iOS/olympguide/olympguide/Application/Scenes/ProfileScene/ProfileTableViewCell.swift
+++ clients/iOS/olympguide/olympguide/Application/Scenes/ProfileScene/ProfileTableViewCell.swift
@@ -5,7 +5,7 @@
 //  Created by Tom Tim on 10.01.2025.
 //
 
-import UIKit
+import UIKit 
 
 class ProfileTableViewCell: UITableViewCell {
     
@@ -14,52 +14,58 @@ class ProfileTableViewCell: UITableViewCell {
     private let label = UILabel()
     private let detailLabel = UILabel()
     private let chevronImageView = UIImageView(image: UIImage(systemName: "chevron.right"))
-    
-    private let separatorLine: UIView = {
-        let view = UIView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = UIColor(hex: "#E7E7E7")
-        return view
-    }()
-    
+    private let separatorLine = UIView()
+
+    // MARK: - Инициализация
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        // Настраиваем сабвью
         label.font = UIFont(name: "MontserratAlternates-Medium", size: 15)!
+        
         detailLabel.font = UIFont(name: "MontserratAlternates-Regular", size: 15)!
         detailLabel.textColor = .gray
+        
+        chevronImageView.tintColor = .black
+        
+        separatorLine.backgroundColor = UIColor(hex: "#E7E7E7")
+        
+        // Добавляем сабвью один раз
+        contentView.addSubview(label)
+        contentView.addSubview(detailLabel)
+        contentView.addSubview(chevronImageView)
+        contentView.addSubview(separatorLine)
+        
+        // Задаём констрейны один раз
+        setupConstraints()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    
+    // MARK: - Публичный метод для конфигурации
     func configure(title: String, detail: String? = nil) {
-        contentView.addSubview(separatorLine)
-        contentView.addSubview(label)
-        contentView.addSubview(detailLabel)
-        contentView.addSubview(chevronImageView)
-        
-        chevronImageView.setHeight(22)
-        chevronImageView.setWidth(13)
-        chevronImageView.pinCenterY(to: contentView)
-        chevronImageView.pinRight(to: contentView.trailingAnchor, 20)
-        chevronImageView.tintColor = .black
-        
         label.text = title
         detailLabel.text = detail
         
+        // Если detail нет, можно скрыть detailLabel или настроить констрейны
+        // Но важный момент: не добавляйте subviews и не создавайте заново констрейны здесь.
+    }
+    
+    // MARK: - Пример метода для установки констрейнов
+    private func setupConstraints() {
+        chevronImageView.pinCenterY(to: contentView.centerYAnchor)
+        chevronImageView.pinRight(to: contentView.trailingAnchor, 20)
+        chevronImageView.setWidth(13)
+        chevronImageView.setHeight(22)
         
         label.pinTop(to: contentView.topAnchor, 21)
         label.pinLeft(to: contentView.leadingAnchor, 20)
-
-        if detail != nil {
-            detailLabel.pinTop(to: label.bottomAnchor, 4)
-            detailLabel.pinLeft(to: contentView.leadingAnchor, 20)
-            detailLabel.pinBottom(to: contentView.bottomAnchor, 21)
-        } else {
-            label.pinBottom(to: contentView.bottomAnchor, 21)
-        }
+        
+        detailLabel.pinTop(to: label.bottomAnchor, 4)
+        detailLabel.pinLeft(to: contentView.leadingAnchor, 20)
+        detailLabel.pinBottom(to: contentView.bottomAnchor, 21)
         
         separatorLine.pinLeft(to: contentView.leadingAnchor, 20)
         separatorLine.pinRight(to: contentView.trailingAnchor, 20)
@@ -67,7 +73,6 @@ class ProfileTableViewCell: UITableViewCell {
         separatorLine.setHeight(1)
     }
     
-    // MARK: - Methods
     func hideSeparator(_ hide: Bool) {
         separatorLine.isHidden = hide
     }
