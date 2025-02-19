c586ef7b60db20e8686c0fa932b2a9362ea3f6b9
sundayti <pankratovvlad1@gmail.com>
2025-02-20 00:48:29 +0300

    Create ProfileProtocols.swift

---
+++ clients/iOS/olympguide/olympguide/Application/Scenes/ProfileScene/VIP/ProfileProtocols.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileProtocols.swift
+//  olympguide
+//
+//  Created by Tom Tim on 19.02.2025.
+//
+
+// MARK: - Routing Logic
+protocol ProfileRoutingLogic {
+    func routeToSignIn()
+    func routeToSignUp()
+}
+
