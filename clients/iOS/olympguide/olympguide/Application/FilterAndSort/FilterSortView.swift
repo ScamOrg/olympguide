ad934dd9202deac57004075f9cb2727c37273ffc
sundayti <pankratovvlad1@gmail.com>
2025-02-20 13:17:20 +0300

    Update FilterSortView.swift

--- clients/iOS/olympguide/olympguide/Application/FilterAndSort/FilterSortView.swift
+++ clients/iOS/olympguide/olympguide/Application/FilterAndSort/FilterSortView.swift
@@ -27,7 +27,7 @@ fileprivate enum Constants {
 
 protocol FilterSortViewDelegate: AnyObject {
     func filterSortViewDidTapSortButton(_ view: FilterSortView)
-    func filterSortView(_ view: FilterSortView, didTapFilterWithTitle title: String)
+    func filterSortView(_ view: FilterSortView, didTapFilterWith title: String)
 }
 
 final class FilterSortView: UIView {
@@ -123,16 +123,17 @@ final class FilterSortView: UIView {
         button.tintColor = Constants.Colors.tintColor
         button.setWidth(Constants.Dimensions.sortButtonSize)
         button.setHeight(Constants.Dimensions.sortButtonSize)
-        button.addTarget(self, action: #selector(sortButtonTapped), for: .touchUpInside)
+//        button.addTarget(self, action: #selector(sortButtonTapped), for: .touchUpInside)
         return button
     }
     
     private func createFilterButton(with title: String) -> UIButton {
-        let button = FilterButton(title: title)
-        //        button.isSelectedItem.toggle()
-        button.tintColor = .black
-        button.addTarget(self, action: #selector(filterButtonTapped), for: .touchUpInside)
-        return button
+//        let button = FilterButton(title: title)
+//        //        button.isSelectedItem.toggle()
+//        button.tintColor = .black
+////        button.addTarget(self, action: #selector(filterButtonTapped), for: .touchUpInside)
+//        return button
+        UIButton(type: .system)
     }
     
     // MARK: - Objc funcs
@@ -140,122 +141,16 @@ final class FilterSortView: UIView {
         delegate?.filterSortViewDidTapSortButton(self)
     }
     
-    @objc private func filterButtonTapped(_ sender: UIButton) {
-        guard let filterButton = sender as? ScrolledButtonProtocol else { return }
-        let title = filterButton.filterTitle
-        delegate?.filterSortView(self, didTapFilterWithTitle: title)
-    }
+//    @objc private func filterButtonTapped(_ sender: UIButton) {
+//        guard let filterButton = sender as? FilterButton else { return }
+//        let title = filterButton.filterTitle
+//        
+//        delegate?.filterSortView(self, didTapFilterWith: title)
+//    }
 }
 
 
 protocol SelectedBarDelegate: UIViewController {
     func toggleCustomTextField()
     func unselectOption(at index: Int)
-}
-
-final class SelectedScrollView: UIView {
-    
-    // MARK: - Variables
-    weak var delegate: SelectedBarDelegate?
-    
-    private lazy var horizontalScrollView: UIScrollView = {
-        let scrollView = UIScrollView()
-        scrollView.showsHorizontalScrollIndicator = false
-        return scrollView
-    }()
-    
-    private lazy var horizontalStackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.axis = .horizontal
-        stackView.spacing = Constants.Dimensions.stackViewSpacing
-        stackView.alignment = .center
-        return stackView
-    }()
-    
-    init (selectedOptions: [String]) {
-        super.init(frame: .zero)
-        setupUI()
-        configure(with: selectedOptions)
-    }
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setupUI()
-    }
-    
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
-        setupUI()
-    }
-    
-    // MARK: - Methods
-    private func setupUI() {
-        backgroundColor = .clear
-        addSubview(horizontalScrollView)
-        horizontalScrollView.addSubview(horizontalStackView)
-        
-        horizontalScrollView.pinLeft(to: leadingAnchor)
-        horizontalScrollView.pinRight(to: trailingAnchor)
-        horizontalScrollView.pinTop(to: topAnchor)
-        horizontalScrollView.pinBottom(to: bottomAnchor)
-        
-        horizontalStackView.pinLeft(to: horizontalScrollView.leadingAnchor, Constants.Dimensions.scrollViewInset)
-        horizontalStackView.pinRight(to: horizontalScrollView.trailingAnchor, Constants.Dimensions.scrollViewInset)
-        horizontalStackView.pinTop(to: horizontalScrollView.topAnchor)
-        horizontalStackView.pinBottom(to: horizontalScrollView.bottomAnchor)
-        horizontalStackView.pinHeight(to: horizontalScrollView)
-    }
-    
-    private func configure(with selectedOptions: [String]) {
-        let space = UIView()
-        space.setWidth(Constants.Dimensions.spaceWidth)
-        horizontalStackView.addArrangedSubview(space)
-        
-        for item in selectedOptions {
-            let filterButton = createSelectedButton(with: item)
-            horizontalStackView.addArrangedSubview(filterButton)
-        }
-    }
-    
-    private func createSelectedButton(with title: String, tag: Int = 0) -> UIButton {
-        let button = FilterButton(title: title)
-        button.tag = tag
-        button.isSelectedItem.toggle()
-        button.tintColor = .black
-        button.addTarget(self, action: #selector(filterButtonTapped), for: .touchUpInside)
-        return button
-    }
-    
-    // MARK: - Objc funcs
-    @objc private func filterButtonTapped(_ sender: UIButton) {
-        sender.isHidden = true
-        self.horizontalStackView.removeArrangedSubview(sender)
-        sender.removeFromSuperview()
-        if self.horizontalStackView.arrangedSubviews.count == 1 {
-            delegate?.toggleCustomTextField()
-        }
-        delegate?.unselectOption(at: sender.tag)
-    }
-    
-    func addButtonToStackView(with title: String, tag: Int) {
-        let selectedButton = createSelectedButton(with: title, tag: tag)
-        horizontalStackView.addArrangedSubview(selectedButton)
-        if self.horizontalStackView.arrangedSubviews.count == 2 {
-            layoutIfNeeded()
-            delegate?.toggleCustomTextField()
-        }
-    }
-    
-    func removeButtons(with tag: Int) {
-        for button in self.horizontalStackView.arrangedSubviews {
-            if let button = button as? FilterButton, button.tag == tag {
-                button.isHidden = true
-                self.horizontalStackView.removeArrangedSubview(button)
-                button.removeFromSuperview()
-                if self.horizontalStackView.arrangedSubviews.count == 1 {
-                    delegate?.toggleCustomTextField()
-                }
-            }
-        }
-    }
 }
