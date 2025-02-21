e9cdf8a08a887b948560768253ce6e0f67da50e3
sundayti <pankratovvlad1@gmail.com>
2025-02-21 23:44:54 +0300

    Update UniversitiesWorker.swift

--- clients/iOS/olympguide/olympguide/Application/Scenes/Universities/UniversitiesScene/VIP/UniversitiesWorker.swift
+++ clients/iOS/olympguide/olympguide/Application/Scenes/Universities/UniversitiesScene/VIP/UniversitiesWorker.swift
@@ -9,51 +9,41 @@ import Foundation
 
 class UniversitiesWorker {
     
-    private enum Constants {
-        static let baseURL: String = ""
-        static let invalidURLMessage: String = "Invalid URL"
-        static let decodingErrorMessage: String = "Decoding error"
+    private let networkService: NetworkServiceProtocol
+    
+    init(networkService: NetworkServiceProtocol = NetworkService()) {
+        self.networkService = networkService
     }
-    
-    private let decoder: JSONDecoder = JSONDecoder()
-    
-    func fetchUniversities(regionID: Int?, sort: String?, search: String?, completion: @escaping (Result<[UniversityModel], Error>) -> Void) {
+
+    func fetchUniversities(
+        with params: Dictionary<String, Set<String>>,
+        completion: @escaping (Result<[UniversityModel], Error>) -> Void
+    ) {
         var queryItems = [URLQueryItem]()
-        if let regionID = regionID {
-            queryItems.append(URLQueryItem(name: "region_id", value: "\(regionID)"))
-        }
-        if let sort = sort {
-            queryItems.append(URLQueryItem(name: "sort", value: sort))
-        }
-        if let search = search {
-            queryItems.append(URLQueryItem(name: "search", value: search))
-        }
-
-        var urlComponents = URLComponents(string: Constants.baseURL)
-        urlComponents?.queryItems = queryItems
-        guard let url = urlComponents?.url else {
-            completion(.failure(NSError(domain: Constants.invalidURLMessage, code: 0)))
-            return
-        }
-
-        URLSession.shared.dataTask(with: url) { data, _, error in
-            if let error = error {
-                completion(.failure(error))
-                return
-            }
-
-            guard let data = data else {
-                completion(.failure(NSError(domain: "No data", code: 0)))
-                return
-            }
-
-            do {
-                let universities = try self.decoder.decode([UniversityModel].self, from: data)
+//        if let levels = levels {
+//            for level in levels {
+//                queryItems.append(URLQueryItem(name: "level", value: "\(level)"))
+//            }
+//        }
+//        if let sort = sort {
+//            queryItems.append(URLQueryItem(name: "sort", value: sort))
+//        }
+//        if let search = search {
+//            queryItems.append(URLQueryItem(name: "search", value: search))
+//        }
+        
+        networkService.request(
+            endpoint: "/universities",
+            method: .get,
+            queryItems: queryItems,
+            body: nil
+        ) { (result: Result<[UniversityModel], NetworkError>) in
+            switch result {
+            case .success(let universities):
                 completion(.success(universities))
-            } catch {
-                print("Decoding error: \(error)")
-                completion(.failure(NSError(domain: Constants.decodingErrorMessage, code: 0)))
+            case .failure(let error):
+                completion(.failure(error))
             }
-        }.resume()
+        }
     }
 }
