6ec4a0390c0afdefb0933d85059293c9b8485161
sundayti <pankratovvlad1@gmail.com>
2025-02-21 23:45:02 +0300

    Create UniversityInteractor.swift

---
+++ clients/iOS/olympguide/olympguide/Application/Scenes/Universities/UniversityScene/VIP/UniversityInteractor.swift
@@ -0,0 +1,40 @@
+//
+//  UniversityInteractor.swift
+//  olympguide
+//
+//  Created by Tom Tim on 20.02.2025.
+//
+
+import Foundation
+
+final class UniversityInteractor: UniversityBusinessLogic, UniversityDataStore {
+    var universityID: Int?
+    
+    var presenter: UniversityPresentationLogic?
+    
+    var worker: UniversityWorkerLogic = UniversityWorker()
+    
+    func loadUniversity(with request: University.Load.Request) {
+        universityID = request.universityID
+        worker.fetchUniverity(
+            with: request.universityID
+        ) { [weak self] result in
+            switch result {
+            case .success(let university):
+                let response = University.Load.Response(
+                    error: nil,
+                    site: university.site,
+                    email: university.email
+                )
+                self?.presenter?.presentLoadUniversity(with: response)
+            case .failure(let error):
+                let response = University.Load.Response(
+                    error: error,
+                    site: nil,
+                    email: nil
+                )
+                self?.presenter?.presentLoadUniversity(with: response)
+            }
+        }
+    }
+}
