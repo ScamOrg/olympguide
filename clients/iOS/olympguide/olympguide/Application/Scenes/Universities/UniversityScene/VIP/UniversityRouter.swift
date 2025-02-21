703fba8adaebbfeb7f0cef029ab4330d64e332fc
sundayti <pankratovvlad1@gmail.com>
2025-02-21 23:44:32 +0300

    Create UniversityRouter.swift

---
+++ clients/iOS/olympguide/olympguide/Application/Scenes/Universities/UniversityScene/VIP/UniversityRouter.swift
@@ -0,0 +1,19 @@
+//
+//  UniversityRouter.swift
+//  olympguide
+//
+//  Created by Tom Tim on 20.02.2025.
+//
+
+import Foundation
+
+import UIKit
+
+final class Router: UniversityRoutingLogic, UniversityDataPassing {
+    var dataStore: UniversityDataStore?
+    
+    func routeTo() {
+        
+    }
+}
+
