3b4386de7d271d1da6c40a6de4e99eb22462f6b3
sundayti <pankratovvlad1@gmail.com>
2025-02-22 20:41:23 +0300

    Update UniversityWorker.swift

--- clients/iOS/olympguide/olympguide/Application/Scenes/Universities/UniversityScene/VIP/UniversityWorker.swift
+++ clients/iOS/olympguide/olympguide/Application/Scenes/Universities/UniversityScene/VIP/UniversityWorker.swift
@@ -12,6 +12,12 @@ protocol UniversityWorkerLogic {
         with universityID: Int,
         completion: @escaping (Result<UniversityModel, NetworkError>) -> Void
     )
+    
+    func toggleFavorite(
+        with universityID: Int,
+        isFavorite: Bool,
+        completion: @escaping (Result<BaseServerResponse, NetworkError>) -> Void
+    )
 }
 
 class UniversityWorker : UniversityWorkerLogic {
@@ -35,6 +41,27 @@ class UniversityWorker : UniversityWorkerLogic {
             switch result {
             case .success(let olympiads):
                 completion(.success(olympiads))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    func toggleFavorite(
+        with universityID: Int,
+        isFavorite: Bool,
+        completion: @escaping (Result<BaseServerResponse, NetworkError>) -> Void
+    ) {
+        let method: HTTPMethod = isFavorite ? .post : .delete
+        networkService.request(
+            endpoint: "/user/favourite/university/\(universityID)",
+            method: method,
+            queryItems: nil,
+            body: nil
+        ) { (result: Result<BaseServerResponse, NetworkError>) in
+            switch result {
+            case .success(let baseResponse):
+                completion(.success(baseResponse))
             case .failure(let error):
                 completion(.failure(error))
             }
