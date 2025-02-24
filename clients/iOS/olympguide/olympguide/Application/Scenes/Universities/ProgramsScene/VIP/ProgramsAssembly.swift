906d13864789bdc86bbd8f3f745df7cf57ee2301
sundayti <pankratovvlad1@gmail.com>
2025-02-25 02:20:59 +0300

    Update ProgramsAssembly.swift

--- clients/iOS/olympguide/olympguide/Application/Scenes/Universities/ProgramsScene/VIP/ProgramsAssembly.swift
+++ clients/iOS/olympguide/olympguide/Application/Scenes/Universities/ProgramsScene/VIP/ProgramsAssembly.swift
@@ -8,7 +8,18 @@
 import UIKit
 
 final class ProgramAssembly {
-    static func build() -> UIViewController {
-        UIViewController()
+    static func build(for univesityID: Int) -> UIViewController {
+        let viewController = ProgramsByFieldsViewController(for: univesityID)
+        let interactor = ProgramInteractor()
+        var presenter = ProgramPresenter()
+        var router = ProgramsRouter()
+        
+        viewController.interactor = interactor
+        interactor.presenter = presenter
+        presenter.viewController = viewController
+        viewController.router = router
+        router.viewController = viewController
+        
+        return viewController
     }
 }
