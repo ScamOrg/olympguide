cfc262b1dfead38a8e0ed734401b975187ec0383
sundayti <pankratovvlad1@gmail.com>
2025-02-24 23:22:04 +0300

    Update program_loader.py
    Add faculty parsing

--- data_loader_service/hse_loader/educational_programs_loader/program_loader.py
+++ data_loader_service/hse_loader/educational_programs_loader/program_loader.py
@@ -1,6 +1,10 @@
 from bs4 import BeautifulSoup
 from EducationalProgram import EducationalProgram
 from utils import fetch_html
+from clients.get_client import get_faculties
+import logging
+
+logger = logging.getLogger(__name__)
 
 def get_edu_program_container(soup: BeautifulSoup):
     return soup.find("div", class_="edu-programm__tab edu-programm__bachelor")
@@ -18,11 +22,19 @@ def extract_program_link_and_name(program_div) -> tuple[str, str]:
         program_href = 'https://design.hse.ru/ba/program/art'
     return program_name, program_href
 
-def extract_faculty(program_div) -> str:
+def extract_faculty(program_div, exist_faculties) -> str:
     faculty_span = program_div.find("span", class_="grey")
+    result = []
     if faculty_span:
-        return faculty_span.text.strip().replace("\xa0", " ")
-    return "Неизвестный факультет"
+        faculties =  faculty_span.text.strip().replace("\xa0", " ").split(',')
+        for faculty in faculties:
+            faculty_strip = faculty.strip().lower()
+            if faculty_strip in exist_faculties:
+                result.append(exist_faculties[faculty_strip])
+            else:
+                logger.error(f"Факультета {faculty_strip} нет")
+        return result
+    return [0]
 
 def extract_places(program_div) -> tuple[int, int]:
     places_div = program_div.find("div", class_="edu-program-places")
@@ -39,19 +51,20 @@ def extract_places(program_div) -> tuple[int, int]:
                 paid_places = int(places[0]) if places[0].isdigit() else 0
     return budget_places, paid_places
 
-def parse_educational_programs_with_faculties() -> dict:
+def parse_educational_programs_with_faculties() -> dict[str: EducationalProgram]:
     url = 'https://www.hse.ru/education/msk/bachelor/'
     soup = fetch_html(url)
     data = {}
     container = get_edu_program_container(soup)
+    exist_faculties = get_faculties(1)
     if container:
         programs = container.find_all("div", class_="b-row edu-programm__item")
         for program_div in programs:
             if not is_moscow_program(program_div):
                 continue
             program_name, program_href = extract_program_link_and_name(program_div)
-            faculty_name = extract_faculty(program_div)
+            faculty_name = extract_faculty(program_div, exist_faculties)
             budget_places, paid_places = extract_places(program_div)
-            edu_program = EducationalProgram(program_name, budget_places, paid_places, program_href, [faculty_name])
+            edu_program = EducationalProgram(program_name, budget_places, paid_places, program_href, faculty_name)
             data[program_name] = edu_program
     return data
