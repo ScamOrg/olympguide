b422ca83efb359c41dc5950c82145004056fb48f
sundayti <pankratovvlad1@gmail.com>
2025-02-24 17:19:40 +0300

    Update ProfileRouter.swift

--- clients/iOS/olympguide/olympguide/Application/Scenes/ProfileScene/VIP/ProfileRouter.swift
+++ clients/iOS/olympguide/olympguide/Application/Scenes/ProfileScene/VIP/ProfileRouter.swift
@@ -16,8 +16,8 @@ final class ProfileRouter: ProfileRoutingLogic {
     }
     
     func routeToSignUp() {
-//        let enterEmailVC = EnterEmailAssembly.build()
-        let enterEmailVC = PersonalDataAssembly.build(email: "")
+        let enterEmailVC = EnterEmailAssembly.build()
+//        let enterEmailVC = PersonalDataAssembly.build(email: "")  
         viewController?.navigationController?.pushViewController(enterEmailVC, animated: true)
     }
 }
