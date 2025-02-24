63013eeef41d0cc2da6a0d9af8827065f6a49164
sundayti <pankratovvlad1@gmail.com>
2025-02-24 17:19:36 +0300

    Create DirectionsAssembly.swift

---
+++ clients/iOS/olympguide/olympguide/Application/Scenes/Universities/DirectionsScene/VIP/DirectionsAssembly.swift
@@ -0,0 +1,14 @@
+//
+//  DirectionsAssembly.swift
+//  olympguide
+//
+//  Created by Tom Tim on 24.02.2025.
+//
+
+import UIKit
+
+final class Assembly {
+    static func build() -> UIViewController {
+        UIViewController()
+    }
+}
