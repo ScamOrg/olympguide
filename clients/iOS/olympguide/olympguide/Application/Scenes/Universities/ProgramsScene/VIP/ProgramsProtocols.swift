e5831c08cb6b80a5bec5b6cd8d8ed13868eaf4e2
sundayti <pankratovvlad1@gmail.com>
2025-02-25 02:22:40 +0300

    Update ProgramsProtocols.swift
    Rename Protocols

--- clients/iOS/olympguide/olympguide/Application/Scenes/Universities/ProgramsScene/VIP/ProgramsProtocols.swift
+++ clients/iOS/olympguide/olympguide/Application/Scenes/Universities/ProgramsScene/VIP/ProgramsProtocols.swift
@@ -6,33 +6,34 @@
 //
 
 import Foundation
+
 // MARK: - Business Logic
-protocol BusinessLogic {
-    func fetchDirections(request: Programs.Load.Request)
+protocol ProgramsBusinessLogic {
+    func loadPrograms(with request: Programs.Load.Request)
 }
 
 // MARK: - Data Store
-// Храним данные, которые могут потребоваться при переходе на другой экран
-protocol DataStore {
-    
+protocol ProgramsDataStore {
+    var groupsOfProgramsByFieldModel: [GroupOfProgramsByFieldModel] { get }
 }
 
 // MARK: - Presentation Logic
-protocol PresentationLogic {
-    func presentActioon(response: Programs.Load.Response)
+protocol ProgramsPresentationLogic {
+    func presentLoadPrograms(with response: Programs.Load.Response)
 }
 
 // MARK: - Display Logic
-protocol DisplayLogic: AnyObject {
-    func displayActionResult(viewModel: Programs.Load.ViewModel)
+protocol ProgramsDisplayLogic: AnyObject {
+    func displayLoadProgramsResult(with viewModel: Programs.Load.ViewModel)
 }
 
 // MARK: - Routing Logic
-protocol RoutingLogic {
-    func routeTo()
+protocol ProgramsRoutingLogic {
+    func routeToProgram()
+    func routeToSearch()
 }
 
 // MARK: - Data Passing
-protocol DataPassing {
-    var dataStore: DataStore? { get }
+protocol ProgramsDataPassing {
+    var dataStore: ProgramsDataStore? { get }
 }
