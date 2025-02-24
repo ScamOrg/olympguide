7e79deb90701e6b4c0fc6afe5c51d43c4a1655e0
sundayti <pankratovvlad1@gmail.com>
2025-02-24 17:19:29 +0300

    Create DirectionsPresenter.swift

---
+++ clients/iOS/olympguide/olympguide/Application/Scenes/Universities/DirectionsScene/VIP/DirectionsPresenter.swift
@@ -0,0 +1,14 @@
+//
+//  DirectionsPresenter.swift
+//  olympguide
+//
+//  Created by Tom Tim on 24.02.2025.
+//
+
+final class Presenter : PresentationLogic {
+    weak var viewController: DisplayLogic?
+    
+    func presentActioon(response: Models.Action.Response) {
+        
+    }
+}
