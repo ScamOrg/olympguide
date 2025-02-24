c70089340b7da0ba8c258228d730f42ba6060718
sundayti <pankratovvlad1@gmail.com>
2025-02-25 02:22:29 +0300

    Update ProgramsPresenter.swift
    Realize presentLoadPrograms

--- clients/iOS/olympguide/olympguide/Application/Scenes/Universities/ProgramsScene/VIP/ProgramsPresenter.swift
+++ clients/iOS/olympguide/olympguide/Application/Scenes/Universities/ProgramsScene/VIP/ProgramsPresenter.swift
@@ -5,10 +5,37 @@
 //  Created by Tom Tim on 24.02.2025.
 //
 
-final class ProgramPresenter : PresentationLogic {
-    weak var viewController: DisplayLogic?
+import UIKit
+
+final class ProgramPresenter : ProgramsPresentationLogic {
+    weak var viewController: (ProgramsDisplayLogic & UIViewController)?
     
-    func presentActioon(response: Programs.Load.Response) {
+    func presentLoadPrograms(with response: Programs.Load.Response) {
+        if let error = response.error {
+            viewController?.showAlert(with: error.localizedDescription)
+            return
+        }
         
+        if let groupsOfPrograms = response.groupsOfPrograms {
+            let groupsOfProgramsViewModel = groupsOfPrograms.map { groupOfPrograms in
+                Programs.Load.ViewModel.GroupOfProgramsViewModel(
+                    name: groupOfPrograms.name,
+                    code: groupOfPrograms.code,
+                    programs: groupOfPrograms.programs.map { program in
+                        Programs.Load.ViewModel.GroupOfProgramsViewModel.ProgramViewModel(
+                            name: program.name,
+                            code: program.field,
+                            budgetPlaces: String(program.budgetPlaces),
+                            paidPlaces: String(program.paidPlaces),
+                            cost: String(program.cost),
+                            requiredSubjects: program.requiredSubjects,
+                            optionalSubjects: program.optionalSubjects
+                        )
+                    }
+                )
+            }
+            let viewModel = Programs.Load.ViewModel(groupsOfPrograms: groupsOfProgramsViewModel)
+            viewController?.displayLoadProgramsResult(with: viewModel)
+        }
     }
 }
