d88867c56a4babef68ab68ce24ec3ea2123e7a99
sundayti <pankratovvlad1@gmail.com>
2025-02-26 03:21:34 +0300

    Update ProgramsInteractor.swift

--- clients/iOS/olympguide/olympguide/Application/Scenes/ProgramsScenes/ProgramsByFieldsScene/VIP/ProgramsInteractor.swift
+++ clients/iOS/olympguide/olympguide/Application/Scenes/ProgramsScenes/ProgramsByFieldsScene/VIP/ProgramsInteractor.swift
@@ -7,17 +7,22 @@
 
 import Foundation
 final class ProgramInteractor: ProgramsBusinessLogic, ProgramsDataStore {
-    var groupsOfProgramsByFieldModel: [GroupOfProgramsModel] = []
+    var groupsOfPrograms: [GroupOfProgramsModel]?
     var presenter: ProgramsPresentationLogic?
     var worker: ProgramsWorkerLogic?
+    var university: UniversityModel?
     
     func loadPrograms(with request: Programs.Load.Request) {
-        worker?.fetch(
+        guard let university = request.university else {return}
+        self.university = request.university
+        worker?.loadPrograms(
             with: request.params,
-            for: request.universityID
+            for: university.universityID,
+            by: request.groups
         ) { [weak self] result in
             switch result {
             case .success(let programs):
+                self?.groupsOfPrograms = programs
                 let response = Programs.Load.Response(groupsOfPrograms: programs, error: nil)
                 self?.presenter?.presentLoadPrograms(with: response)
             case .failure(let error):
