20a0aa8aba484468f02145bd30d2a0093caa4f23
sundayti <pankratovvlad1@gmail.com>
2025-02-26 03:22:26 +0300

    Update UniversityInteractor.swift
    Add ProgramsBusinessLogic and ProgramsDataStore protocols

--- clients/iOS/olympguide/olympguide/Application/Scenes/Universities/UniversityScene/VIP/UniversityInteractor.swift
+++ clients/iOS/olympguide/olympguide/Application/Scenes/Universities/UniversityScene/VIP/UniversityInteractor.swift
@@ -7,13 +7,18 @@
 
 import Foundation
 
-final class UniversityInteractor: UniversityBusinessLogic, UniversityDataStore {
+final class UniversityInteractor: UniversityDataStore, ProgramsDataStore {
+    var groupsOfPrograms: [GroupOfProgramsModel]?
+    var university: UniversityModel?
+    
     var universityID: Int?
     
-    var presenter: UniversityPresentationLogic?
+    var presenter: (UniversityPresentationLogic & ProgramsPresentationLogic)?
     
-    var worker: UniversityWorkerLogic = UniversityWorker()
-    
+    var worker = UniversityWorker()
+}
+
+extension UniversityInteractor : UniversityBusinessLogic {
     func loadUniversity(with request: University.Load.Request) {
         universityID = request.universityID
         worker.fetchUniverity(
@@ -54,6 +59,27 @@ final class UniversityInteractor: UniversityBusinessLogic, UniversityDataStore {
                     error: error
                 )
                 self?.presenter?.presentToggleFavorite(with: response)
+            }
+        }
+    }
+}
+    
+extension UniversityInteractor : ProgramsBusinessLogic {
+    func loadPrograms(with request: Programs.Load.Request) {
+        guard let university = request.university else {return}
+        worker.loadPrograms(
+            with: request.params,
+            for: university.universityID,
+            by: request.groups
+        ) { [weak self] result in
+            switch result {
+            case .success(let programs):
+                self?.groupsOfPrograms = programs
+                let response = Programs.Load.Response(groupsOfPrograms: programs, error: nil)
+                self?.presenter?.presentLoadPrograms(with: response)
+            case .failure(let error):
+                let response = Programs.Load.Response(groupsOfPrograms: nil, error: error)
+                self?.presenter?.presentLoadPrograms(with: response)
             }
         }
     }
