7b651ef2f8331628823de2a678c10116721467ee
sundayti <pankratovvlad1@gmail.com>
2025-02-27 18:17:35 +0300

    Update Protocols.swift

--- clients/iOS/olympguide/olympguide/Application/Scenes/ProgramsScenes/FavoriteProgramsScene/VIP/Protocols.swift
+++ clients/iOS/olympguide/olympguide/Application/Scenes/ProgramsScenes/FavoriteProgramsScene/VIP/Protocols.swift
@@ -5,4 +5,32 @@
 //  Created by Tom Tim on 27.02.2025.
 //
 
-import Foundation
+// MARK: - Business Logic
+protocol FavoriteProgramsBusinessLogic {
+    func action(with request: FavoriteProgramsModels.Action.Request)
+}
+
+// MARK: - Data Store
+protocol FavoriteProgramsDataStore {
+    
+}
+
+// MARK: - Presentation Logic
+protocol FavoriteProgramsPresentationLogic {
+    func presentActioon(with response: FavoriteProgramsModels.Action.Response)
+}
+
+// MARK: - Display Logic
+protocol FavoriteProgramsDisplayLogic: AnyObject {
+    func displayActionResult(with viewModel: FavoriteProgramsModels.Action.ViewModel)
+}
+
+// MARK: - Routing Logic
+protocol FavoriteProgramsRoutingLogic {
+    func routeTo()
+}
+
+// MARK: - Data Passing
+protocol FavoriteProgramsDataPassing {
+    var dataStore: FavoriteProgramsDataStore? { get }
+}
