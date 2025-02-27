2c68ba10f9d13317d1f8ad78baa0cd8a7f7ffa14
sundayti <pankratovvlad1@gmail.com>
2025-02-27 19:07:02 +0300

    Update FavoriteProgramsPresenter.swift

--- clients/iOS/olympguide/olympguide/Application/Scenes/ProgramsScenes/FavoriteProgramsScene/VIP/FavoriteProgramsPresenter.swift
+++ clients/iOS/olympguide/olympguide/Application/Scenes/ProgramsScenes/FavoriteProgramsScene/VIP/FavoriteProgramsPresenter.swift
@@ -5,10 +5,31 @@
 //  Created by Tom Tim on 27.02.2025.
 //
 
+import UIKit
+
 final class FavoriteProgramsPresenter : FavoriteProgramsPresentationLogic {
-    weak var viewController: FavoriteProgramsDisplayLogic?
+    weak var viewController: (FavoriteProgramsDisplayLogic & UIViewController)?
     
-    func presentActioon(with response: FavoriteProgramsModels.Action.Response) {
-        <#code#>
+    func presentLoadPrograms(with response: FavoritePrograms.Load.Response) {
+        if let error = response.error {
+            viewController?.showAlert(with: error.localizedDescription)
+        }
+        
+        guard let programs = response.programs else { return }
+        
+        let viewPrograms  = programs.map { program in
+            ProgramViewModel(
+                name: program.name,
+                code: program.field,
+                budgetPlaces: program.budgetPlaces,
+                paidPlaces: program.paidPlaces,
+                cost: program.cost,
+                requiredSubjects: program.requiredSubjects,
+                optionalSubjects: program.optionalSubjects
+            )
+        }
+        
+        let viewModel = FavoritePrograms.Load.ViewModel(programs: viewPrograms)
+        viewController?.displayLoadProgramsResult(with: viewModel)
     }
 }
