240ea349f277d904a11a57a53d24eaf93d5194cf
sundayti <pankratovvlad1@gmail.com>
2025-02-27 18:17:42 +0300

    Create FavoriteProgramsModels.swift

---
+++ clients/iOS/olympguide/olympguide/Application/Scenes/ProgramsScenes/FavoriteProgramsScene/VIP/FavoriteProgramsModels.swift
@@ -0,0 +1,19 @@
+//
+//  Models.swift
+//  olympguide
+//
+//  Created by Tom Tim on 27.02.2025.
+//
+
+enum FavoriteProgramsModels {
+    enum Action {
+        struct Request {
+        }
+        
+        struct Response {
+        }
+        
+        struct ViewModel {
+        }
+    }
+}
