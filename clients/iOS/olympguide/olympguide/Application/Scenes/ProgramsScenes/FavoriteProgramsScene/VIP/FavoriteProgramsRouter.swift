33bdf7fe81fa48c418c1f9506f9bf68d858019bf
sundayti <pankratovvlad1@gmail.com>
2025-02-27 18:17:36 +0300

    Create FavoriteProgramsRouter.swift

---
+++ clients/iOS/olympguide/olympguide/Application/Scenes/ProgramsScenes/FavoriteProgramsScene/VIP/FavoriteProgramsRouter.swift
@@ -0,0 +1,18 @@
+//
+//  Router.swift
+//  olympguide
+//
+//  Created by Tom Tim on 27.02.2025.
+//
+
+import UIKit
+
+final class FavoriteProgramsRouter: FavoriteProgramsRoutingLogic, FavoriteProgramsDataPassing {
+    var dataStore: FavoriteProgramsDataStore?
+    var viewController: UIViewController?
+    
+    func routeTo() {
+        <#code#>
+    }
+}
+
