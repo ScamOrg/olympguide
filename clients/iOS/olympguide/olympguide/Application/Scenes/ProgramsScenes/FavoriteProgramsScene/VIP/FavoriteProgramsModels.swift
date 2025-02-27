0b355711cb357003dae527924af607bde7df4591
sundayti <pankratovvlad1@gmail.com>
2025-02-27 19:06:58 +0300

    Update FavoriteProgramsModels.swift

--- clients/iOS/olympguide/olympguide/Application/Scenes/ProgramsScenes/FavoriteProgramsScene/VIP/FavoriteProgramsModels.swift
+++ clients/iOS/olympguide/olympguide/Application/Scenes/ProgramsScenes/FavoriteProgramsScene/VIP/FavoriteProgramsModels.swift
@@ -5,15 +5,18 @@
 //  Created by Tom Tim on 27.02.2025.
 //
 
-enum FavoriteProgramsModels {
-    enum Action {
+enum FavoritePrograms {
+    enum Load {
         struct Request {
         }
         
         struct Response {
+            var error: Error? = nil
+            var programs: [ProgramModel]? = nil
         }
         
         struct ViewModel {
+            let programs: [ProgramViewModel]
         }
     }
 }
