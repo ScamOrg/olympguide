b31ed4d28eef4403d96ef1f5251787569568822b
sundayti <pankratovvlad1@gmail.com>
2025-02-27 18:17:40 +0300

    Create FavoriteProgramsPresenter.swift

---
+++ clients/iOS/olympguide/olympguide/Application/Scenes/ProgramsScenes/FavoriteProgramsScene/VIP/FavoriteProgramsPresenter.swift
@@ -0,0 +1,14 @@
+//
+//  Presenter.swift
+//  olympguide
+//
+//  Created by Tom Tim on 27.02.2025.
+//
+
+final class FavoriteProgramsPresenter : FavoriteProgramsPresentationLogic {
+    weak var viewController: FavoriteProgramsDisplayLogic?
+    
+    func presentActioon(with response: FavoriteProgramsModels.Action.Response) {
+        <#code#>
+    }
+}
