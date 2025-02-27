d61ea774767ff10682b5bc1274dc31b0c1f11e45
sundayti <pankratovvlad1@gmail.com>
2025-02-27 21:58:41 +0300

    Update ProgramAssembly.swift

--- clients/iOS/olympguide/olympguide/Application/Scenes/ProgramsScenes/ProgramScene/VIP/ProgramAssembly.swift
+++ clients/iOS/olympguide/olympguide/Application/Scenes/ProgramsScenes/ProgramScene/VIP/ProgramAssembly.swift
@@ -24,4 +24,18 @@ final class ProgramAssembly {
         
         return viewContoller
     }
+    
+    static func build(for program: ProgramModel) -> UIViewController {
+        let viewContoller = ProgramViewController(for: program)
+        let interactor = ProgramInteractor()
+        let presenter = ProgramPresenter()
+        let worker = ProgramWorker()
+        
+        viewContoller.interactor = interactor
+        interactor.presenter = presenter
+        interactor.worker = worker
+        presenter.viewController = viewContoller
+        
+        return viewContoller
+    }
 }
