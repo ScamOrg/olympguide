901638df15fc75ad953588d7a58466fac490a887
sundayti <pankratovvlad1@gmail.com>
2025-02-27 19:06:50 +0300

    Update FavoriteProgramsAssembly.swift

--- clients/iOS/olympguide/olympguide/Application/Scenes/ProgramsScenes/FavoriteProgramsScene/VIP/FavoriteProgramsAssembly.swift
+++ clients/iOS/olympguide/olympguide/Application/Scenes/ProgramsScenes/FavoriteProgramsScene/VIP/FavoriteProgramsAssembly.swift
@@ -9,6 +9,20 @@ import UIKit
 
 final class FavoriteProgramsAssembly {
     static func build() -> UIViewController {
-        UIViewController()
+        let viewController = FavoriteProgramsViewController()
+        let interactor = FavoriteProgramsInteractor()
+        let presenter = FavoriteProgramsPresenter()
+        let router = FavoriteProgramsRouter()
+        let worker = FavoriteProgramsWorker()
+        
+        viewController.interactor = interactor
+        viewController.router = router
+        router.viewController = viewController
+        router.dataStore = interactor
+        interactor.presenter = presenter
+        interactor.worker = worker
+        presenter.viewController = viewController
+        
+        return viewController
     }
 }
