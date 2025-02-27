8c5c3d742018ffb00fb98f591bdab36e0f6f992d
sundayti <pankratovvlad1@gmail.com>
2025-02-27 17:27:25 +0300

    Create ProgramProtocols.swift

---
+++ clients/iOS/olympguide/olympguide/Application/Scenes/ProgramsScenes/ProgramScene/VIP/ProgramProtocols.swift
@@ -0,0 +1,42 @@
+//
+//  Protocols.swift
+//  olympguide
+//
+//  Created by Tom Tim on 26.02.2025.
+//
+
+import Foundation
+
+// MARK: - Business Logic
+protocol ProgramBusinessLogic {
+    func loadProgram(with request: Program.Load.Request)
+    func toggleFavorite(request: Program.Favorite.Request)
+}
+
+// MARK: - Data Store
+// Храним данные, которые могут потребоваться при переходе на другой экран
+protocol ProgramDataStore {
+    
+}
+
+// MARK: - Presentation Logic
+protocol ProgramPresentationLogic {
+    func presentLoadProgram(with response: Program.Load.Response)
+    func presentToggleFavorite(response: Program.Favorite.Response)
+}
+
+// MARK: - Display Logic
+protocol ProgramDisplayLogic: AnyObject {
+    func displayLoadProgram(with viewModel: Program.Load.ViewModel)
+    func displayToggleFavoriteResult(viewModel: Program.Favorite.ViewModel)
+}
+
+// MARK: - Routing Logic
+protocol ProgramRoutingLogic {
+    func routeTo()
+}
+
+// MARK: - Data Passing
+protocol ProgramDataPassing {
+    var dataStore: ProgramDataStore? { get }
+}
