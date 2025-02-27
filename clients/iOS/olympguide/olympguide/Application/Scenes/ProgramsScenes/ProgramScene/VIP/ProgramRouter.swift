84636309ebaea89e07e631a03f353f080e096f17
sundayti <pankratovvlad1@gmail.com>
2025-02-27 17:27:27 +0300

    Create ProgramRouter.swift

---
+++ clients/iOS/olympguide/olympguide/Application/Scenes/ProgramsScenes/ProgramScene/VIP/ProgramRouter.swift
@@ -0,0 +1,18 @@
+//
+//  Router.swift
+//  olympguide
+//
+//  Created by Tom Tim on 26.02.2025.
+//
+
+import UIKit
+
+final class ProgramRouter: ProgramRoutingLogic, ProgramDataPassing {
+    weak var viewController: UIViewController?
+    var dataStore: ProgramDataStore?
+    
+    func routeTo() {
+        
+    }
+}
+
