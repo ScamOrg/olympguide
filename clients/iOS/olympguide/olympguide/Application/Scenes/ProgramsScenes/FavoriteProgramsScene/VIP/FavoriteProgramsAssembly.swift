79e25db939a9c29cfe47c39b540c7fb7d741b393
sundayti <pankratovvlad1@gmail.com>
2025-02-27 18:17:46 +0300

    Create FavoriteProgramsAssembly.swift

---
+++ clients/iOS/olympguide/olympguide/Application/Scenes/ProgramsScenes/FavoriteProgramsScene/VIP/FavoriteProgramsAssembly.swift
@@ -0,0 +1,14 @@
+//
+//  Assembly.swift
+//  olympguide
+//
+//  Created by Tom Tim on 27.02.2025.
+//
+
+import UIKit
+
+final class FavoriteProgramsAssembly {
+    static func build() -> UIViewController {
+        UIViewController()
+    }
+}
